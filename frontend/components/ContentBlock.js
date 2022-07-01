3cf638e6655b6ec7dd1639cbb9f5179289103062
maria besteman <maria.besteman@witteveenbos.com>
2022-07-01 12:41:15 +0200

    basic ContentBlock functions

---
+++ frontend/components/ContentBlock.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from 'next/link'; 
+import Button from './Button'; 
+
+
+
+
+function ContentBlock(content) {
+
+    let styling = (content.styling) ? content.styling : ""; 
+
+    let id = (content.id) ? content.id : ""; 
+
+    let bgColor = content.colorClass; 
+    let linkname = (content.linkname) ? content.linkname : ""; 
+
+    return (
+        
+        <div className={`snap-start w-screen h-screen + ${bgColor} flex justify-center items-center`} id={id}>
+            <p className="text-lg">Er is hier een voorbeeldtekst met een 
+            <Link href={linkname}> link
+            </Link></p>
+        </div>
+    )
+}; 
+
+export default ContentBlock; 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
+import ContentBlock from "../components/ContentBlock";
+
 function Card({ children, href }) {
   return (
     <a
@@ -24,64 +26,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex h-screen flex-1 flex-col items-center justify-center py-6">
-        <h1 className="text-6xl">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="mt-6 text-lg">
-          <Link href="tailwind-example">
-            <a
-              href="tailwind-example"
-              className="group flex items-center gap-0.5 p-3 hover:no-underline"
-            >
-              <Fragment>
-                <span className="group-hover:underline">See an example with Tailwind</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 transition group-hover:translate-x-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </Fragment>
-            </a>
-          </Link>
-        </p>
-
-        <p className="my-6 text-center text-xl">
-          Get started by editing{" "}
-          <code className="rounded bg-gray-100 px-2 py-1">pages/index.js</code>
-        </p>
-
-        <div className="flex max-w-3xl flex-wrap items-center justify-center">
-          <Card href="https://nextjs.org/docs">
-            <h2 className="mb-3 text-2xl">Documentation &rarr;</h2>
-            <p className="text-lg">Find in-depth information about Next.js features and API.</p>
-          </Card>
-
-          <Card href="https://nextjs.org/learn">
-            <h2 className="mb-3 text-2xl">Learn &rarr;</h2>
-            <p className="text-lg">Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-
-          <Card href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2 className="mb-3 text-2xl">Examples &rarr;</h2>
-            <p className="text-lg">Discover and deploy boilerplate example Next.js projects.</p>
-          </Card>
-
-          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2 className="mb-3 text-2xl">Deploy &rarr;</h2>
-            <p className="text-lg">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </Card>
-        </div>
+      <main className="snap-y snap-mandatory h-screen overflow-scroll">
+      <ContentBlock colorClass="bg-cyan-400" id="blue" linkname="#purple">
+      </ContentBlock>
+      <ContentBlock colorClass="bg-rose-400" id="rose" linkname="#red">
+      </ContentBlock>
+      <ContentBlock colorClass="bg-purple-400" id="purple" linkname="#blue">
+      </ContentBlock>
+      <ContentBlock colorClass="bg-red-400" id="red" linkname="#rose">
+      </ContentBlock>      
       </main>
 
       <footer className="mb-3 text-lg">
