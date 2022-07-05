d050124494f212935fe3c297360dfcec1181a421
Mattijs Stam <32770108+mattijsstam@users.noreply.github.com>
2022-07-05 09:57:58 +0200

    Update update_register.py
    Door niet de hele modelviewset vanuit rest framework in te laden maar alleen de GenericViewSet en de CreateModelMixin kun je afvangen dat alleen een post mogelijk is zonder zelf permissions te moeten schrijven.

--- backend/holon/viewsets/update_register.py
+++ backend/holon/viewsets/update_register.py
@@ -1,10 +1,8 @@
 from holon.models import UpdateRegister
 from holon.serializers import UpdateRegisterSerializer
-from rest_framework import viewsets, permissions
-from holon.permissions import AllowOnlyPost
+from rest_framework import viewsets, mixins
 
 # ViewSets define the view behavior.
-class UpdateRegisterViewSet(viewsets.ModelViewSet):
+class UpdateRegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = UpdateRegister.objects.all()
     serializer_class = UpdateRegisterSerializer
-    permission_classes = [AllowOnlyPost]
