d96de37bfa3a4562c1037dfa3b5c19aac617d491
stam4 <mattijs.stam@witteveenbos.com>
2022-07-07 16:18:54 +0200

    Fixes next image optimization conflict with export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  images: {
+    loader: 'imgix',
+    path: 'https://holontool.nl',
+    domains: ['holontool.nl']
+  }
 };
 
 module.exports = nextConfig;
