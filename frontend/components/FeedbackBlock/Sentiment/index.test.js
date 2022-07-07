Zenmo/Holon-webapp

commit 9db0d2aab6112ca5a5bfba9494854f39eab2c0c3

PUT request when there is a choice already made

frontend/components/FeedbackBlock/Sentiment/index.js
@@ -1,27 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { RadioGroup } from "@headlessui/react";
 import EmoticonButton from "./EmoticonButton";
 
 export default function Sentiment() {
-  const [choice, setChoice] = useState();
+  const [choice, setChoice] = useState(null);
+
+  useEffect(() => {
+    setChoice(localStorage.getItem("holon_sentiment_value") || null)
+  })
 
   const onChange = (selected) => {
     if (!selected) {
       return;
     }
 
-    setChoice(selected);
+    let method = "POST"
+    let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/rating/`
+    if (localStorage.getItem("holon_sentiment_value") && localStorage.getItem("holon_sentiment_id")) {
+      method = "PUT"
+      url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/rating/${localStorage.getItem("holon_sentiment_id")}/`
+    }
 
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/rating/`, {
+    fetch(url, {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      method: "POST",
+      method: method,
       body: JSON.stringify({ rating: selected.toUpperCase() }),
     })
-      .then(() => {})
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+      })
+      .then(json => {
+        localStorage.setItem("holon_sentiment_value", (json.rating).toLowerCase())
+        localStorage.setItem("holon_sentiment_id", json.id)
+      })
+      .then(() => setChoice(selected))
       .catch(() => {});
   };
 

backend/holon/serializers/rating.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class RatingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Rating
-        fields = ["datetime", "rating"]
+        fields = ["id", "datetime", "rating"]

backend/holon/viewsets/rating.py
@@ -3,6 +3,10 @@ from holon.serializers import RatingSerializer
 from rest_framework import viewsets, mixins
 
 # ViewSets define the view behavior.
-class RatingViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class RatingViewSet(
+    mixins.CreateModelMixin, 
+    mixins.UpdateModelMixin, 
+    viewsets.GenericViewSet
+):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer

commit e5fe1ccac6c941bc3362dad70dd5616e32c1c355

Fix Sentiment test, use jest-fetch-mock
- Fixes test that was broken in 9db0d2a.

- Adds jest-fetch-mock for more reliable mocking of requests to the
  backend.

frontend/.env.test
@@ -1,0 +1,1 @@
+NEXT_PUBLIC_BACKEND_URL=

frontend/components/FeedbackBlock/Sentiment/index.js
@@ -10,13 +10,14 @@ export default function Sentiment() {
     setChoice(localStorage.getItem("holon_sentiment_value") || null);
   }, []);
 
-  const onChange = (selected) => {
+  const onChange = async (selected) => {
     if (!selected) {
       return;
     }
 
     let method = "POST";
     let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/rating/`;
+
     if (
       localStorage.getItem("holon_sentiment_value") &&
       localStorage.getItem("holon_sentiment_id")
@@ -27,26 +28,29 @@ export default function Sentiment() {
       )}/`;
     }
 
-    fetch(url, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: method,
-      body: JSON.stringify({ rating: selected.toUpperCase() }),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("HTTP error " + response.status);
-        }
-        return response.json();
-      })
-      .then((json) => {
-        localStorage.setItem("holon_sentiment_value", json.rating.toLowerCase());
-        localStorage.setItem("holon_sentiment_id", json.id);
-      })
-      .then(() => setChoice(selected))
-      .catch(() => {});
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: method,
+        body: JSON.stringify({ rating: selected.toUpperCase() }),
+      });
+
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+
+      const json = await response.json();
+
+      localStorage.setItem("holon_sentiment_value", json.rating.toLowerCase());
+      localStorage.setItem("holon_sentiment_id", json.id);
+
+      setChoice(selected);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (

frontend/jest.setup.js
@@ -5,3 +5,7 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
+
+import JestFetchMock from "jest-fetch-mock";
+
+JestFetchMock.enableMocks();

frontend/components/FeedbackBlock/Sentiment/index.test.js
@@ -1,18 +1,9 @@
-import { render, screen, fireEvent, prettyDOM } from "@testing-library/react";
+import { render, screen, fireEvent, prettyDOM, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 import Sentiment from ".";
 
 describe("Sentiment", () => {
-  beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({}),
-      })
-    );
-  });
-
-  afterEach(jest.restoreAllMocks);
-
   it("renders with no option selected", () => {
     render(<Sentiment />);
 
@@ -25,26 +16,42 @@ describe("Sentiment", () => {
     });
   });
 
-  it("allows selecting an option", () => {
-    const screen = render(<Sentiment />);
-    const radios = screen.queryAllByRole("radio");
-
-    fireEvent.click(radios[0]);
-
-    expect(radios).toHaveLength(4);
-    expect(radios[0].getAttribute("aria-checked")).toEqual("true");
-  });
-
   describe("when submitting a sentiment", () => {
-    it("sends data to the API", () => {
-      render(<Sentiment />);
+    beforeEach(() => {
+      fetchMock.mockResponse(JSON.stringify({ id: 1, rating: "HEART" }));
+    });
 
-      fireEvent.click(screen.getAllByRole("radio")[0]);
+    afterEach(() => {
+      fetchMock.mockRestore();
+    });
 
-      expect(global.fetch).toHaveBeenCalledTimes(1);
+    it("allows selecting an option", async () => {
+      const screen = render(<Sentiment />);
+      const radios = screen.queryAllByRole("radio");
+
+      await act(async () => {
+        return await fireEvent.click(radios[0]);
+      });
+
+      await waitFor(() => {
+        expect(radios[0].getAttribute("aria-checked")).toEqual("true");
+      });
+    });
+
+    it("sends data to the API", async () => {
+      const screen = render(<Sentiment />);
+      const radios = screen.queryAllByRole("radio");
+
+      await act(async () => {
+        return await fireEvent.click(radios[1]);
+      });
+
+      await waitFor(() => {
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+      });
 
       const sentData = JSON.parse(global.fetch.mock.calls[0][1].body);
-      expect(sentData).toEqual({ rating: "HEART" });
+      expect(sentData).toEqual({ rating: "THUMBSUP" });
     });
   });
 });

frontend/components/FeedbackBlock/SubscriptionForm.test.js
@@ -9,6 +9,8 @@ async function completeForm({ name, email, company }) {
   const companyEl = screen.getByLabelText(/Bedrijf/);
   const submitEl = screen.getByRole("button");
 
+  fetchMock.mockResponse(JSON.stringify({ name, company, email }));
+
   return await act(async () => {
     fireEvent.change(nameEl, { target: { value: name } });
     fireEvent.change(emailEl, { target: { value: email } });
@@ -19,17 +21,13 @@ async function completeForm({ name, email, company }) {
 }
 
 describe("SubscriptionForm", () => {
-  afterEach(jest.restoreAllMocks);
+  afterEach(() => {
+    fetchMock.mockRestore();
+  });
 
   describe("with a name and e-mail", () => {
     beforeEach(() => {
       render(<SubscriptionForm />);
-
-      global.fetch = jest.fn(() =>
-        Promise.resolve({
-          json: () => Promise.resolve({}),
-        })
-      );
     });
 
     it("submits the data to an API endpoint", async () => {