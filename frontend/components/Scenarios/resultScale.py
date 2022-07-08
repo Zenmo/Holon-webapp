bfa521f00d19f90c7ef9c5067c97d2ab9ab66b6c
thesethtruth <s.w.p.vanwieringen@gmail.com>
2022-07-08 10:53:51 +0200

    scenario update resultcolorscale

--- frontend/components/Scenarios/resultScale.py
+++ frontend/components/Scenarios/resultScale.py
@@ -1,8 +1,11 @@
+#%%
 import json
 import seaborn as sns
 
-scale = sns.diverging_palette(10, 115, s=60, l=60, as_cmap=False, n=101, sep=1)
-scale = scale.as_hex()
+scale_low = sns.diverging_palette(10, 60, s=60, l=60, as_cmap=False, n=50, sep=1)
+scale_high = sns.diverging_palette(60, 115, s=60, l=60, as_cmap=False, n=51, sep=1)
+
+scale = [*scale_low.as_hex(), *scale_high.as_hex()]
 s = json.dumps(scale)
 s = "export const resultScale = " + s + ";"
 with open("resultScale.js", "w") as outfile:
