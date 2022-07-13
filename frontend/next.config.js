1a7d38447822b504c4783ca175713374f08f2efc
maria besteman <maria.besteman@witteveenbos.com>
2022-07-13 14:49:53 +0200

    redirects /inbreng to #feedback

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
       unoptimized: true,
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/inbreng',
+        destination: '/#feedback',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
