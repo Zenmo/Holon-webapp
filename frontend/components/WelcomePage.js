0f8ad23d8c3995333da695aaec524d42a8538ef1
Anthony Williams <hi@antw.dev>
2022-07-14 13:56:25 +0100

    Adjust logos on the welcome page
    - Reduces their size at all viewport widths
    - Positions logos absolutely at the bottom of the content block

--- frontend/components/WelcomePage.js
+++ frontend/components/WelcomePage.js
@@ -15,23 +15,23 @@ export default function WelcomePage() {
         <h1 className="mx-6 pb-3 text-center text-6xl font-semibold shadow-golden">Welkom!</h1>
         <p className="mt-8 text-xl">HOLON en de kunst van het loslaten</p>
       </div>
-      <div className="mx-16 mt-4 flex flex-wrap justify-center">
+      <div className="mx-16 mt-4 mb-24 flex flex-wrap justify-center">
         <div className="w-1/2 p-8 pb-0">
           <h2 className="pb-3 text-4xl shadow-golden lg:text-5xl xl:text-6xl">Energietransitie</h2>
-          <p className="my-8 w-4/5 text-lg">
+          <p className="my-8 w-4/5 text-base lg:text-lg">
             We hebben nu een centraal gestuurd energiesysteem. Dat is aan het veranderen. Hoe ziet
             het energiesysteem van de toekomst eruit?
           </p>
         </div>
         <div className="w-1/2 p-8 pb-0">
           <h2 className="pb-3 text-4xl shadow-blue lg:text-5xl xl:text-6xl">Russisch gas</h2>
-          <p className="my-8 w-4/5 text-lg">
+          <p className="my-8 w-4/5 text-base lg:text-lg">
             We willen snel een alternatief voor aardgas uit Rusland. Welke mogelijkheden zijn er
             binnen Europa en wat is de rol van Nederland?
           </p>
         </div>
         <div className="w-1/2 p-8 pt-0">
-          <HolonButton tag="a" variant="gold" href="#introVideo">
+          <HolonButton tag="a" variant="gold" href="#introVideo" className="text-base">
             Vertel me meer!
           </HolonButton>
         </div>
@@ -42,23 +42,26 @@ export default function WelcomePage() {
             target="_blank"
             rel="noreferrer noopener"
             variant="darkblue"
+            className="text-base"
           >
             Naar de tool
           </HolonButton>
         </div>
       </div>
-      <div className="mx-24 my-16 flex h-11 flex-wrap items-center justify-between">
-        <div className="w-1/2 px-8 lg:w-1/4">
-          <Image alt="Logo Zenmo" src={zenmoLogo} />
-        </div>
-        <div className="w-1/2 px-8 lg:w-1/4">
-          <Image alt="Logo TNO" src={tnoLogo} />
-        </div>
-        <div className="w-1/2 px-8 lg:w-1/4">
-          <Image alt="Logo Witteveen+Bos" src={wbLogo} />
-        </div>
-        <div className="w-1/2 px-8 lg:w-1/4">
-          <Image alt="Logo Quintel" src={quintelLogo} />
+      <div className="absolute bottom-0 left-0 right-0 pb-6">
+        <div className="mx-auto flex max-w-sm flex-wrap items-center justify-center sm:max-w-3xl">
+          <div className="w-1/2 px-6 sm:w-1/4">
+            <Image alt="Logo Zenmo" src={zenmoLogo} />
+          </div>
+          <div className="w-1/2 px-6 sm:w-1/4">
+            <Image alt="Logo TNO" src={tnoLogo} />
+          </div>
+          <div className="w-1/2 px-6 sm:w-1/4">
+            <Image alt="Logo Witteveen+Bos" src={wbLogo} />
+          </div>
+          <div className="w-1/2 px-6 sm:w-1/4">
+            <Image alt="Logo Quintel" src={quintelLogo} />
+          </div>
         </div>
       </div>
     </div>
