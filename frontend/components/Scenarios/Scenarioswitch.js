cb2f0781f6af7aa6fec6a49ca2d7bd1b02df01dd
maria besteman <maria.besteman@witteveenbos.com>
2022-07-15 11:32:22 +0200

    adds visual disabled state sliders

--- frontend/components/Scenarios/Scenarioswitch.js
+++ frontend/components/Scenarios/Scenarioswitch.js
@@ -13,6 +13,7 @@ function ScenarioSwitch({
   label,
   scenarioid,
   message,
+  colorSwitch = locked ? "after:bg-slated-500" : "after:bg-holon-blue-500", 
 }) {
   return (
     <div className="flex flex-row items-center justify-end gap-2">
@@ -38,7 +39,7 @@ function ScenarioSwitch({
             onChange={(e) => updatevalue(inputid, e.target.checked)}
             checked={value}
           />
-          <div className="bg-yellow h-[30px] w-16 rounded-sm border-2 border-holon-blue-900 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:border-holon-blue-900 after:bg-holon-blue-500 after:shadow-[2px_2px_0_0]   after:transition-all after:content-[''] peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300"></div>
+          <div className={`bg-yellow h-[30px] w-16 rounded-sm border-2 border-holon-blue-900 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:border-holon-blue-900 ${colorSwitch} after:shadow-[2px_2px_0_0]   after:transition-all after:content-[''] peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300`}></div>
         </span>
         <small>{on}</small>
       </label>
