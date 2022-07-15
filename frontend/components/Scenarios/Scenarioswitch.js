04a0bae3f94ab3486ddd4378a62c12ed8f49392c
maria besteman <maria.besteman@witteveenbos.com>
2022-07-15 13:37:49 +0200

    small changes

--- frontend/components/Scenarios/Scenarioswitch.js
+++ frontend/components/Scenarios/Scenarioswitch.js
@@ -38,7 +38,11 @@ function ScenarioSwitch({
             onChange={(e) => updatevalue(inputid, e.target.checked)}
             checked={value}
           />
-          <div className={`bg-yellow h-[30px] w-16 rounded-sm border-2 border-holon-blue-900 0 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] ${disabled ? "after:bg-holon-grey-300" : "after:bg-holon-blue-500"}  after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:border-holon-blue-900 after:shadow-[2px_2px_0_0] after:transition-all after:content-[''] peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300`}></div>
+          <div
+            className={`bg-yellow h-[30px] w-16 rounded-sm border-2 border-holon-blue-900 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] ${
+              disabled ? "after:bg-holon-grey-300" : "after:bg-holon-blue-500"
+            }  after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:border-holon-blue-900 after:shadow-[2px_2px_0_0] after:transition-all after:content-[''] peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300`}
+          ></div>
         </span>
         <small>{on}</small>
       </label>
