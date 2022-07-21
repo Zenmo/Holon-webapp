1d24f0c4a7246c29bc936cc283ba215efde9b182
stam4 <mattijs.stam@witteveenbos.com>
2022-07-21 15:28:04 +0200

    Adds holontool.nl to trusted origins

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,11 +1,17 @@
 from .base import *
 
-ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+ALLOWED_HOSTS = (
+    "holontool.nl" + [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+)
 CSRF_TRUSTED_ORIGINS = (
-    ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+    "https://holontool.nl" + ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+    if "WEBSITE_HOSTNAME" in os.environ
+    else []
 )
 CORS_ALLOWED_ORIGINS = (
-    ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+    "https://holontool.nl" + ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+    if "WEBSITE_HOSTNAME" in os.environ
+    else []
 )
 
 DEBUG = False
