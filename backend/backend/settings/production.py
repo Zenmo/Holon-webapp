029d4db7bce59943af711e8e05e68529494cbada
stam4 <mattijs.stam@witteveenbos.com>
2022-07-21 13:58:18 +0200

    Adds production logging

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -36,3 +36,26 @@ DATABASES = {
         "PASSWORD": os.environ["DBPASS"],
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        }
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "WARNING"),
+        },
+    },
+}
