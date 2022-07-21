c1b186aef69c37363fda86a6e9eb395b73468c05
stam4 <mattijs.stam@witteveenbos.com>
2022-07-21 15:43:19 +0200

    Fixes for real

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -1,18 +1,14 @@
 from .base import *
 
-ALLOWED_HOSTS = (
-    "holontool.nl" + [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
-)
+ALLOWED_HOSTS = ([os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []) + [
+    "holontool.nl"
+]
 CSRF_TRUSTED_ORIGINS = (
-    "https://holontool.nl" + ["https://" + os.environ["WEBSITE_HOSTNAME"]]
-    if "WEBSITE_HOSTNAME" in os.environ
-    else []
-)
+    ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+) + ["https://holontool.nl"]
 CORS_ALLOWED_ORIGINS = (
-    "https://holontool.nl" + ["https://" + os.environ["WEBSITE_HOSTNAME"]]
-    if "WEBSITE_HOSTNAME" in os.environ
-    else []
-)
+    ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+) + ["https://holontool.nl"]
 
 DEBUG = False
 
