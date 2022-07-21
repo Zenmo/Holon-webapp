7126f75311c62f472d37affc2d02f9225703c7cc
maria besteman <maria.besteman@witteveenbos.com>
2022-07-21 14:33:08 +0200

    fix style switch disabled

--- frontend/components/Scenarios/Scenarioswitch.js
+++ frontend/components/Scenarios/Scenarioswitch.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Tooltip from "./Tooltip";
 
 function ScenarioSwitch({
-  disabled,
+  locked,
   neighbourhoodID,
   inputid,
   value,
@@ -28,9 +28,9 @@ function ScenarioSwitch({
         className="flex items-center gap-1"
       >
         <small>{off}</small>
-        <span className={` relative mx-1 ${disabled && "cursor-not-allowed"}`}>
+        <span className={` relative mx-1 ${locked && "cursor-not-allowed"}`}>
           <input
-            disabled={disabled}
+            disabled={locked}
             type="checkbox"
             value=""
             id={`scenario${inputid}switch${neighbourhoodID}${scenarioid}`}
@@ -39,11 +39,11 @@ function ScenarioSwitch({
             checked={value}
           />
           <div
-            className={`h-[30px] w-16 rounded-sm border-2 border-holon-blue-900 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] ${
-              disabled
+            className={`${
+              locked
                 ? "border-gray-500 text-slate-500 shadow-gray-500 after:border-gray-500 after:bg-holon-grey-300"
-                : "after:bg-holon-blue-500"
-            }  after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:border-holon-blue-900 after:shadow-[2px_2px_0_0] after:transition-all after:content-['']  peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%]  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300`}
+                : "border-holon-blue-900 after:border-holon-blue-900 after:bg-holon-blue-500"
+            }  h-[30px] w-16 rounded-sm border-2 p-1 text-holon-blue-900 shadow-[4px_4px_0_0] after:absolute after:top-[4px] after:left-[2px]  after:h-[1.3rem] after:w-5 after:rounded-sm after:border-2 after:shadow-[2px_2px_0_0] after:transition-all after:content-['']  peer-checked:after:left-[-2px] peer-checked:after:translate-x-[200%]  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300`}
           ></div>
         </span>
         <small>{on}</small>
