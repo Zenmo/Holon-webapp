3c8d7cfaac384b7d53c56bd9d8e1466a9bdd45ff
maria besteman <maria.besteman@witteveenbos.com>
2022-07-26 14:59:54 +0200

    adds effect

--- frontend/pages/inbreng.js
+++ frontend/pages/inbreng.js
@@ -2,6 +2,10 @@ import React, { Component, useEffect } from "react";
 import Link from "next/link";
 
 function Inbreng() {
+  useEffect(() => {
+    window.location.href = "https://nl.surveymonkey.com/r/RYK7SRL";
+  }, []);
+
   return (
     <div className="prose mt-6 ml-6">
       <h1>Je wordt doorgestuurd naar de enquête</h1>
