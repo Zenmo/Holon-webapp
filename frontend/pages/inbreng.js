ad06d443bf38b6a0da428a7a5e26865c044bdf3b
maria besteman <maria.besteman@witteveenbos.com>
2022-07-26 14:54:27 +0200

    changes text inbrengPage and some styling

--- frontend/pages/inbreng.js
+++ frontend/pages/inbreng.js
@@ -1,14 +1,16 @@
 import React, { Component, useEffect } from "react";
+import Link from "next/link";
 
 function Inbreng() {
-  useEffect(() => {
-    window.location.href = "https://nl.surveymonkey.com/r/RYK7SRL";
-  }, []);
-
   return (
-    <div>
-      <h1>This page is not available</h1>
-      <p>You are redirected to Surveymonkey</p>
+    <div className="prose mt-6 ml-6">
+      <h1>Je wordt doorgestuurd naar de enquête</h1>
+      <p>
+        Indien je niet automatisch wordt doorgestuurd klik dan op deze{""}
+        <Link href="https://nl.surveymonkey.com/r/RYK7SRL">
+          <a>link naar de enquête</a>
+        </Link>
+      </p>
     </div>
   );
 }
