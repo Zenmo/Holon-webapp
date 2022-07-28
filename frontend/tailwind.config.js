27289c5f7501795220a295f40d9a7b5093c0e228
Anthony Williams <hi@antw.dev>
2022-07-28 11:09:43 +0100

    Remove Tailwind safelists
    - The "order" classname is no longer needed since the welcome page was
      changed.
    
    - Including the "sm" variant resulted in *every* `*-sm` class being
      included in the CSS. This resulted in large CSS and long compile times
      in development.
    
    Closes #75

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    {
-      pattern: /order-/,
-      variants: ["sm"],
-    },
-  ],
   theme: {
     extend: {
       colors: {
