89e3adbad96bd2593b1d80b393298a0dc9c8e656
stam4 <mattijs.stam@witteveenbos.com>
2022-07-29 13:31:27 +0000

    initial devcontainer setup

---
+++ .devcontainer/init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+cd /workspace/backend
+python manage.py migrate
+
+cd /workspace/frontend
+npm install 
+
+exec "$@"
