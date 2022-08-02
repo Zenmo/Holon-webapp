89f60d226e3e7afeede1d842f261fbe240ce4cb8
maria besteman <maria.besteman@witteveenbos.com>
2022-08-02 15:26:32 +0200

    removes testMode from gtag.js

--- frontend/util/gtag.js
+++ frontend/util/gtag.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
-  ReactGA.initialize("G-EHMXV36KJV", { testMode: true });
+  ReactGA.initialize("G-EHMXV36KJV");
   ReactGA.pageview(window.location.pathname + window.location.search);
   ReactGA.ga("send", "pageview", "/mypage");
 };
