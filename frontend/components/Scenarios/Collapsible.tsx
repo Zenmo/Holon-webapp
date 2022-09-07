d77828380377d99c4fc44f0181ad7126e13dda5c
maria besteman <maria.besteman@witteveenbos.com>
2022-09-07 12:48:40 +0000

    adds test Collapsible component

--- frontend/components/Scenarios/Collapsible.tsx
+++ frontend/components/Scenarios/Collapsible.tsx
@@ -18,13 +18,19 @@ export default function Collapsible({ children, label }: Props) {
       <h4
         className={`toggle relative my-4 border-l-[0.75rem] border-b-2 border-holon-blue-900 pl-3 text-xl font-light `}
         onClick={toggleSection}
+        data-testid={label}
       >
         {label}
         <span className={`absolute right-0 h-4 w-4 text-black md:hidden`}>
           {isOpen ? <ChevronUpIcon></ChevronUpIcon> : <ChevronDownIcon></ChevronDownIcon>}
         </span>
       </h4>
-      <div className={`${isOpen ? "flex" : "hidden"} mb-4 flex-col gap-4 md:flex`}>{children}</div>
+      <div
+        className={`${isOpen ? "flex" : "hidden"} mb-4 flex-col gap-4 md:flex`}
+        data-testid={`section-${label}`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
