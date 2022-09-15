d682e1698a4e75d937bce692a0f7b88a8e70e8b4
ALFP <pascal.alferink@witteveenbos.com>
2022-09-15 06:26:48 +0000

    work-in-progress

---
+++ frontend/components/wiki/Aside.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+import React from "react";
+
+interface Props {
+    posts: IPostItem[];
+}
+
+interface IPostItem {
+    name: string,
+    url: string,
+    children: IPostItem[],
+}
+
+interface IFolderItem {
+    folderItem: {
+        name: string,
+        children: IPostItem[],
+    }
+}
+
+interface IDocumentItem {
+    docItem: {
+        name: string,
+        url: string,
+    }
+}
+function Document({ docItem }:IDocumentItem) {
+  const url =
+  docItem.url.indexOf("index.mdx") > 0
+      ? docItem.url.replace(/\index\.mdx$/, "")
+      : docItem.url.replace(/\.mdx$/, "");
+  const name = docItem.name.replace(/\.mdx$/, "");
+  return (
+    <Link href={"/wiki/" + url}>
+      <span className={"block cursor-pointer bg-inherit px-4 pt-1 pb-3 "}>{name}</span>
+    </Link>
+  );
+}
+Document.propTypes = {
+  docItem: PropTypes.shape({
+    name: PropTypes.string,
+    url: PropTypes.string,
+  }),
+};
+
+function Folder({ folderItem }:IFolderItem) {
+    return (
+        <details open className="order-1 p-0 ">
+        <summary className="text-md cursor-pointer bg-inherit py-3 px-4">
+            <strong>{folderItem.name}</strong>
+        </summary>
+        <div className="flex flex-col pl-4">
+            {folderItem.children.map((child, index) =>
+            child.children.length < 1 ? (
+                <Document key={index} docItem={child} />
+            ) : (
+                <Folder key={index} folderItem={child} />
+            )
+            )}
+        </div>
+        </details>
+    );
+}
+Folder.propTypes = {
+    folderItem: PropTypes.shape({
+    name: PropTypes.string,
+    children: PropTypes.any,
+  }),
+};
+
+function Aside({ posts }:Props) {
+    return (
+        <aside className=" sticky top-0 mx-3 mt-5">
+            <div className="">
+                {posts && posts.map((item, index) =>
+                item.children.length < 1 ? (
+                    <Document key={index} docItem={item} />
+                ) : (
+                    <Folder key={index} folderItem={item} />
+                )
+                )}
+            </div>
+        </aside>
+    )
+
+}
+
+export default Aside
+
+Aside.propTypes = {
+  posts: PropTypes.any,
+};
