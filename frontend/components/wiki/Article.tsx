598bb64775842321ff5e91961f7949cd03e79822
Anthony Williams <hi@antw.dev>
2022-09-20 15:25:50 +0100

    Fix types in Article
    Setting the type for `articleComponents` and `sideComponents` fixes type
    errors when passing the components to `MDXProvider`.

--- frontend/components/wiki/Article.tsx
+++ frontend/components/wiki/Article.tsx
@@ -1,69 +1,74 @@
 import { MDXProvider } from "@mdx-js/react";
-import PropTypes from "prop-types";
-import React, { Fragment } from "react";
 
-interface IMDXItemProps {
+const toSlug = (str: string) => {
+  return str
+    .toString()
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
+
+interface MDXItemProps {
   children: string;
 }
 
-const articleComponents = {
-  h1: (props: IMDXItemProps) => <h1 id={"h1-" + encodeURIComponent(props.children)} {...props} />,
-  h2: (props: IMDXItemProps) => <h2 id={"h2-" + encodeURIComponent(props.children)} {...props} />,
-  h3: (props: IMDXItemProps) => <h2 id={"h3-" + encodeURIComponent(props.children)} {...props} />,
-  h4: (props: IMDXItemProps) => <h2 id={"h4-" + encodeURIComponent(props.children)} {...props} />,
-  h5: (props: IMDXItemProps) => <h2 id={"h5-" + encodeURIComponent(props.children)} {...props} />,
-  h6: (props: IMDXItemProps) => <h2 id={"h6-" + encodeURIComponent(props.children)} {...props} />,
+const articleComponents: Record<string, (p: MDXItemProps) => React.ReactElement> = {
+  h1: (props: MDXItemProps) => <h1 id={toSlug(props.children)} {...props} />,
+  h2: (props: MDXItemProps) => <h2 id={toSlug(props.children)} {...props} />,
+  h3: (props: MDXItemProps) => <h3 id={toSlug(props.children)} {...props} />,
+  h4: (props: MDXItemProps) => <h4 id={toSlug(props.children)} {...props} />,
+  h5: (props: MDXItemProps) => <h5 id={toSlug(props.children)} {...props} />,
+  h6: (props: MDXItemProps) => <h6 id={toSlug(props.children)} {...props} />,
 };
 
-const sidecomponents = {
-  h1: (props: IMDXItemProps) =>
-    typeof typeof props.children !== "object" && (
+const sideComponents: Record<string, (p: MDXItemProps) => React.ReactElement | null> = {
+  h1: (props: MDXItemProps) =>
+    typeof typeof props.children === "string" ? (
       <a
         className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h1-" + encodeURIComponent(props.children)}
+        href={`#${toSlug(props.children)}`}
         {...props}
       />
+    ) : (
+      <></>
     ),
-  h2: (props: IMDXItemProps) =>
-    typeof props.children !== "object" && (
+  h2: (props: MDXItemProps) =>
+    typeof props.children === "string" ? (
       <a
         className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h2-" + encodeURIComponent(props.children)}
+        href={`#${toSlug(props.children)}`}
         {...props}
       />
-    ),
-  h3: (props: IMDXItemProps) =>
-    typeof props.children !== "object" && (
+    ) : null,
+  h3: (props: MDXItemProps) =>
+    typeof props.children === "string" ? (
       <a
         className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h3-" + encodeURIComponent(props.children)}
+        href={`#${toSlug(props.children)}`}
         {...props}
       />
-    ),
-  h4: (props: IMDXItemProps) =>
-    typeof props.children !== "object" && (
+    ) : null,
+  h4: (props: MDXItemProps) =>
+    typeof props.children === "string" ? (
       <a
         className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h4-" + encodeURIComponent(props.children)}
+        href={`#${toSlug(props.children)}`}
         {...props}
       />
-    ),
-  h5: (props: IMDXItemProps) =>
-    typeof props.children !== "object" && (
+    ) : null,
+  h5: (props: MDXItemProps) =>
+    typeof props.children === "string" ? (
+      <a href={`#${toSlug(props.children)}`} {...props} />
+    ) : null,
+  h6: (props: MDXItemProps) =>
+    typeof props.children === "string" ? (
       <a
         className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h5-" + encodeURIComponent(props.children)}
+        href={`#${encodeURIComponent(props.children)}`}
         {...props}
       />
-    ),
-  h6: (props: IMDXItemProps) =>
-    typeof props.children !== "object" && (
-      <a
-        className="wiki-context-menu-link px-4 pt-1 pb-3"
-        href={"#h6-" + encodeURIComponent(props.children)}
-        {...props}
-      />
-    ),
+    ) : null,
 
   //ignore all other tags
   p: () => null,
@@ -84,7 +89,7 @@ interface ArticleProps {
 
 export default function Article({ article }: ArticleProps) {
   return (
-    <Fragment>
+    <>
       <MDXProvider components={articleComponents}>
         <article className="prose mt-5 ml-10 mb-16 w-3/4">{article}</article>
       </MDXProvider>
@@ -94,14 +99,10 @@ export default function Article({ article }: ArticleProps) {
             <strong>Inhoudsopgave</strong>
           </h3>
           <div className="wiki-context-menu">
-            <MDXProvider components={sidecomponents}>{article}</MDXProvider>
+            <MDXProvider components={sideComponents}>{article}</MDXProvider>
           </div>
         </div>
       </nav>
-    </Fragment>
+    </>
   );
 }
-
-Article.propTypes = {
-  article: PropTypes.shape({}),
-};
