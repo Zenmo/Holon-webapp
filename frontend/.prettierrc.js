12006bd7ce002739d399d1b88c4f5bf7743abf8c
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2022-09-29 09:48:44 +0200

    Some small changes to the frontend

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-    trailingComma: 'es5',
-    printWidth: 100,
-    tabWidth: 2,
-    singleQuote: false,
-    semi: true,
-    jsxBracketSameLine: true,
-    arrowParens: 'always',
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  endOfLine: "lf"
 };
