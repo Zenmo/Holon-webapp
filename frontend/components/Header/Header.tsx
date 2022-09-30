929b9807eb40b686674b174af938bb2a2cb90661
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2022-09-30 08:58:26 +0200

    Temp commit...

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
-// import i18n from '../../i18n';
-// import PropTypes from 'prop-types';
-import styles from './Header.module.css';
+import faviconholon from "../../public/favicon.ico";
+import Navbar from "./Navbar";
 
-const Header = () => <div className={styles['Header']}>Header</div>;
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex h-10 snap-start flex-row items-center bg-white opacity-90">
+      <div className="mx-6 flex h-8 w-8 items-center">
+        <Link href="#introVideo" title="Logo Holon linking to homepage" className="">
+          <Image src={faviconholon} layout="intrinsic"></Image>
+        </Link>
+      </div>
 
-Header.propTypes = {};
-
-Header.defaultProps = {};
-
-export default Header;
+      <span className="ml-4 text-left text-lg font-semibold ">Holon</span>
+      <Navbar />
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "holon-gold-200": "#F3E7C5",
+        "holon-gold-600": "#C89D28",
+        "holon-blue-200": "#C6D8F2",
+        "holon-blue-500": "#23549F",
+        "holon-blue-900": "#051E3F",
+        "holon-grey-300": "#BFBFBF",
+        "holon-slated-blue-300": "#AEC2E6",
+      },
+      backgroundImage: {
+        "split-white-blue": "linear-gradient(-18deg, #051E3F 40% , white 30%)",
+        "split-blue-white": "linear-gradient(-18deg, white 45%, #051E3F 40%)",
+      },
+      borderWidth: {
+        8: "24px",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      translate: {
+        "holon-bh-x": "0.2rem", // holon-bh :: holon-button-hover
+        "holon-bh-y": "0.1rem",
+      },
+      boxShadow: {
+        "holon-white": "0.3rem 0.15rem white",
+        "holon-white-hover": "0.1rem 0.05rem white",
+        "holon-blue": "0.3rem 0.15rem #051E3F",
+        "holon-blue-hover": "0.1rem 0.05rem #051E3F",
+        golden: "inset 0 -25px 0px 0px #F3E7C5",
+        blue: "inset 0 -25px 0px 0px #AEC2E6",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
