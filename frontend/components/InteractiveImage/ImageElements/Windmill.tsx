a005a6d85e4aabcd3c6de2ad6d848ff1654006ab
maria besteman <maria.besteman@witteveenbos.com>
2022-10-11 14:34:09 +0000

    adds interactive image

---
+++ frontend/components/InteractiveImage/ImageElements/Windmill.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  top: string;
+  left: string;
+  width: string;
+  dataTestId?: string;
+}
+
+export default function Windmill({ top, left, width, dataTestId }: Props) {
+  return (
+    <div
+      className={`windmill absolute`}
+      style={{ top, left, width }}
+      data-testid={dataTestId ? dataTestId : ""}>
+      <img
+        src="/imgs/flat_windmill_pole.png"
+        className="windmillPole relative top-0 left-0"
+        alt=""></img>
+      <img
+        src="/imgs/flat_windmill_blades.png"
+        className={`windmillBlades absolute top-[-40%] left-[0%]`}
+        alt=""></img>
+    </div>
+  );
+}
