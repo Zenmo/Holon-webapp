f1eca6c639662a876f2727a6579517edcd9850a6
Anthony Williams <hi@antw.dev>
2022-10-24 16:37:36 +0100

    Fix Storybook examples for some of the Pipit components

--- frontend/containers/WikiPage/WikiPage.stories.tsx
+++ frontend/containers/WikiPage/WikiPage.stories.tsx
@@ -1,13 +1,27 @@
 /* global module */
 
-import React from 'react';
-import WikiPage from './WikiPage';
-import data from './WikiPage.data';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import WikiPage from "./WikiPage";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Components/WikiPage',
-    component: WikiPage,
-};
+  title: "Components/WikiPage",
+  component: WikiPage,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof WikiPage>;
 
-export const WikiPageWithoutData = () => <WikiPage />;
-export const WikiPageWithData = () => <WikiPage {...data} />;
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof WikiPage> = args => <WikiPage {...args} />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  richText: "Hello!",
+  wikiMenu: {
+    items: [],
+    meta: { totalCount: 0 },
+  },
+};
