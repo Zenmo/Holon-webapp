f621eae3b24fa488bff02805a6b95e7eec3c1180
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2022-10-26 17:03:48 +0200

    Fixed Windmills ID test

--- frontend/components/InteractiveImage/ImageElements/Windmill.tsx
+++ frontend/components/InteractiveImage/ImageElements/Windmill.tsx
@@ -4,24 +4,16 @@ interface Props {
   top?: string;
   left?: string;
   width?: string;
-  dataTestId?: string;
 }
 
-export default function Windmill({ top, left, width, dataTestId }: Props) {
+export default function Windmill({ top, left, width }: Props) {
   return (
-    <div
-      className={`${styles.windmill} absolute`}
-      style={{ top, left, width }}
-      data-testid={dataTestId ? dataTestId : ""}>
+    <div className={`${styles.windmill} absolute`} style={{ top, left, width }}>
       <img
-        id="windmills1"
-        data-testid="windmills1"
         src="/imgs/flat_windmill_pole.png"
         className={`${styles.windmillPole} relative top-0 left-0`}
         alt=""></img>
       <img
-        id="windmills2"
-        data-testid="windmills2"
         src="/imgs/flat_windmill_blades.png"
         className={`${styles.windmillBlades} absolute top-[-40%] left-[0%]`}
         alt=""></img>
--- frontend/components/InteractiveImage/ImageElements/Windmills.tsx
+++ frontend/components/InteractiveImage/ImageElements/Windmills.tsx
@@ -3,10 +3,10 @@ import Windmill from "./Windmill";
 
 export default function Windmills() {
   return (
-    <React.Fragment>
+    <div id="windmills1" data-testid="windmills1">
       <Windmill top="64%" left="6%" width="16%" />
       <Windmill top="69%" left="9%" width="16%" />
       <Windmill top="71%" left="15.6%" width="16%" />
-    </React.Fragment>
+    </div>
   );
 }
