a547a3b1749ab68b580fbe46ff195460990c1c75
stam4 <mattijs.stam@witteveenbos.com>
2022-10-28 09:28:13 +0000

    adds validation to slider value

--- src/holon/serializers.py
+++ src/holon/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 from api.models.scenario import Scenario
 from api.models.slider import Slider
+from django.utils.translation import gettext_lazy as _
 
 
 class SliderInputSerializer(serializers.Serializer):
     slider = serializers.PrimaryKeyRelatedField(queryset=Slider.objects.all())
     value = serializers.IntegerField()
+
+    def validate(self, data):
+        slider = data["slider"]
+
+        if data["value"] < slider.slider_value_min or data["value"] > slider.slider_value_max:
+            raise serializers.ValidationError(
+                {
+                    "value": [
+                        _("Slider value is out of range, min %(min)s max %(max)s")
+                        % {"min": slider.slider_value_min, "max": slider.slider_value_max}
+                    ]
+                }
+            )
+        return data
 
     class Meta:
         fields = ["slider", "value"]
