93f9a68e812465b0fab53bc9afeee81b4a2ccd2a
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2022-11-01 10:02:58 +0100

    Styling of header

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,22 +1,46 @@
-import Link from "next/link";
-import Image from "next/image";
-
-import faviconholon from "@/public/favicon.ico";
+import { useState } from "react";
 import Navbar from "./Navbar";
-
 import { NavItem } from "@/api/types";
 
 export default function Header({ navigation }: { navigation: NavItem[] }) {
-  return (
-    <div className="sticky top-0 z-50 flex h-10 snap-start flex-row items-center bg-white opacity-90">
-      <div className="mx-6 flex h-8 w-8 items-center">
-        <Link href="#introVideo" title="Logo Holon linking to homepage" className="">
-          <Image src={faviconholon} layout="intrinsic" alt="favicon"></Image>
-        </Link>
-      </div>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      <span className="ml-4 text-left text-lg font-semibold ">Holon</span>
-      <Navbar items={navigation} />
-    </div>
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 bg-white py-10 px-8 rounded dark:bg-gray-900">
+      <div className="flex flex-wrap justify-between items-center mx-auto">
+        <a href="/" className="flex items-center">
+          <div className=" text-left uppercase text-2xl font-bold text-holon-blue-900">Holon</div>
+        </a>
+        <button
+          onClick={() => handleClick()}
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"></path>
+          </svg>
+        </button>
+        <div
+          className={`${menuOpen ? "" : "hidden"} w-full md:block md:w-auto`}
+          id="navbar-default">
+          <Navbar items={navigation} />
+        </div>
+      </div>
+    </nav>
   );
 }
--- frontend/components/Header/Navbar.tsx
+++ frontend/components/Header/Navbar.tsx
@@ -1,48 +1,20 @@
-import { useState } from "react";
 import Link from "next/link";
-import { Bars3Icon } from "@heroicons/react/24/outline";
 import { NavItem } from "@/api/types";
 
 export default function Navbar({ items }: { items: NavItem[] }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleClick = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <div className="relative flex w-full flex-col items-end justify-end sm:flex-row">
-      <div className="mx-4 flex h-8 w-8 items-center sm:hidden">
-        <button id="header__toggler" onClick={handleClick} className="w-full">
-          <Bars3Icon />
-        </button>
-      </div>
-
-      <div
-        id="header__menu-items"
-        className={`${
-          menuOpen ? "flex flex-col" : "hidden"
-        } absolute top-9 items-end space-x-4 bg-white p-4 text-lg font-bold sm:static sm:mr-4 sm:flex sm:flex-row sm:p-0`}>
-        {items.map((item: NavItem, index) => {
-          return (
-            <Link key={index} href={`/${item.slug}`} rel="noreferrer noopener">
-              <a className="w-full p-2 text-right hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-4 sm:p-0">
+    <ul className="flex flex-col md:flex-row md:space-x-8 md:mt-0">
+      {items.map((item: NavItem, index) => {
+        return (
+          <li key={index}>
+            <Link href={`/${item.slug}`} rel="noreferrer noopener">
+              <div className="w-full cursor-pointer px-2 py-3 md:py-2 text-xl text-holon-blue-900 hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-4 sm:p-0">
                 {item.title}
-              </a>
+              </div>
             </Link>
-          );
-        })}
-        <Link href="/wiki" target="_blank" rel="noreferrer noopener">
-          <a className="w-full p-2 text-right hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-4 sm:p-0">
-            Wiki
-          </a>
-        </Link>
-        <Link href="#feedback">
-          <a className="w-full p-2 text-right hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-4 sm:p-0">
-            Contact
-          </a>
-        </Link>
-      </div>
-    </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
