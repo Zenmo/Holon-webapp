c439ad2ad40acbdf79bba81124361e00df7e4d26
stam4 <mattijs.stam@witteveenbos.com>
2022-11-01 12:10:26 +0000

    Use scenario in calcuations and fix migrations

--- src/holon/views.py
+++ src/holon/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import generics, status
+from holon.cloudclient.run_cloud_experiments.scripts.run_scenario import run_scenario_endpoint
 
 from holon.serializers import HolonRequestSerializer
 from etm_service import retrieve_results
@@ -9,6 +10,24 @@ from pathlib import Path
 ETM_CONFIG_PATH = Path(__file__).resolve().parent / "services"
 ETM_CONFIG_FILE = "etm.config"
 SCENARIO_ID = 1647734
+SCENARIO_HOLON_NAME = "webdev_cloud_poc"
+RESULTS = ["totalElectricityImported_MWh", "totalElectricityExported_MWh"]
+
+
+def format_holon_input(value):
+    CAPACITY_PER_WINDWILL = 100
+    return {
+        "P energy assets config JSON": {
+            "index": 0,
+            "agenttype": "energyAsset",
+            "id": "a1",
+            "type": "PRODUCTION",
+            "type2": "WINDMILL",
+            "parent": "b25",
+            "capacity_electric_kw": value * CAPACITY_PER_WINDWILL,
+            "capacity_heat_kw": 0,
+        }
+    }
 
 
 class HolonService(generics.CreateAPIView):
@@ -19,10 +38,18 @@ class HolonService(generics.CreateAPIView):
         serializer = HolonRequestSerializer(data=request.data)
 
         if serializer.is_valid():
-            print(serializer.validated_data)
-            etm_results = retrieve_results(SCENARIO_ID, ETM_CONFIG_PATH, ETM_CONFIG_FILE)
+            data = serializer.validated_data
+            value = data.get("sliders")[0]["value"]
+            holon_results = run_scenario_endpoint(
+                data.get("scenario").model_name, format_holon_input(value), RESULTS
+            )
+            print(holon_results)
+            etm_results = retrieve_results(
+                data.get("scenario").etm_scenario_id, ETM_CONFIG_PATH, ETM_CONFIG_FILE
+            )
             return Response(
-                {"message": "kpis", "etm_result": etm_results}, status=status.HTTP_200_OK
+                {"message": "kpis", "etm_result": etm_results, "holon_result": holon_results},
+                status=status.HTTP_200_OK,
             )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
