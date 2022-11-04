c250f59d953fafc4583fd258b6f6ceb5fcd3089b
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2022-11-04 14:50:10 +0100

    Removed unneeded factories

--- src/main/factories/scenario_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePageFactory
-from ..pages.scenario import ScenarioPage
-
-
-class ScenarioPageFactory(BasePageFactory):
-    class Meta:
-        model = ScenarioPage
--- src/main/factories/scenariolist_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePageFactory
-from ..pages.scenariolist import ScenariolistPage
-
-
-class ScenariolistPageFactory(BasePageFactory):
-    class Meta:
-        model = ScenariolistPage
