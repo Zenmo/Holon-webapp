cec13891b19621df80c49515a5485f0f6da0ac9f
maria besteman <maria.besteman@witteveenbos.com>
2022-11-15 15:17:42 +0000

    fix outlining buttons cardblock

--- frontend/components/Blocks/CardsBlock/CardBlock.tsx
+++ frontend/components/Blocks/CardsBlock/CardBlock.tsx
@@ -61,16 +61,17 @@ export default function CardBlock({
   },
 }: Props) {
   return (
-    <div
-      className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-12 px-10 lg:px-16 lg:pt-16 gap-8`}>
-      {cards.map((cardItem, index) => {
-        return (
-          <React.Fragment key={index}>
-            <Card cardItem={cardItem}></Card>
-          </React.Fragment>
-        );
-      })}
-
+    <div>
+      <div
+        className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-12 px-10 lg:px-16 lg:pt-16 gap-8`}>
+        {cards.map((cardItem, index) => {
+          return (
+            <React.Fragment key={index}>
+              <Card cardItem={cardItem}></Card>
+            </React.Fragment>
+          );
+        })}
+      </div>
       {buttonBlock.length > 0 && (
         <ButtonBlock
           buttons={buttonBlock[0].value.buttons}
