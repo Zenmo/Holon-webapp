2325285ac3b7627421ab3028ee24248875541a7c
maria besteman <maria.besteman@witteveenbos.com>
2022-11-16 10:22:15 +0000

    adds blocks to intro and footer of storyline overview page

--- src/main/pages/storylineoverview.py
+++ src/main/pages/storylineoverview.py
@@ -5,17 +5,48 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.fields import StreamField
 
 from .base import BasePage
+from ..blocks import TitleBlock, HeroBlock, CardsBlock, TextAndMediaBlock, StorylineSectionBlock
 
 
 class StorylineOverviewPage(HeadlessPreviewMixin, BasePage):
+
+    intro = StreamField(
+        [
+            ("title_block", TitleBlock()),
+            ("hero_block", HeroBlock()),
+            ("text_image_block", TextAndMediaBlock()),
+            ("card_block", CardsBlock()),
+        ],
+        verbose_name="Page intro",
+        blank=True,
+        null=True,
+        use_json_field=True,
+    )
+    
+    footer = StreamField(
+        [
+            ("title_block", TitleBlock()),
+            ("text_image_block", TextAndMediaBlock()),
+            ("card_block", CardsBlock()),
+        ],
+        verbose_name="Page footer",
+        blank=True,
+        null=True,
+        use_json_field=True,
+    )
+
+    content_panels = BasePage.content_panels + [
+        FieldPanel("intro"),
+        FieldPanel("footer"),
+    ]
+
     serializer_class = "main.pages.StorylineOverviewPageSerializer"
 
     parent_page_types = ["main.HomePage"]
     subpage_types = [
         "main.StorylinePage",  # appname.ModelName
     ]
-
-    content_panels = BasePage.content_panels
+    
     extra_panels = BasePage.extra_panels
 
     class Meta:
