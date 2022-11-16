714633b53d25685f42805e62c58abf2826c5554a
ALFP <pascal.alferink@witteveenbos.com>
2022-11-16 12:51:56 +0000

    upate static images

--- frontend/components/VersionOne/Hero/Hero.tsx
+++ frontend/components/VersionOne/Hero/Hero.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
 import React from "react";
-import Image from "next/image";
 
 import s from "./Hero.module.css";
-import logo from "../../../public/img/logo.svg";
 
 const Hero = ({ title }: { title: string }) => (
   <div className={s.Container}>
-    <Image src={logo} width="100" height="100" className={s.Logo} alt="Holon" />
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src="/img/logo.svg" width="100" height="100" className={s.Logo} alt="Holon" />
     <h1 className={s.Title}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img src={"/img/white_circle.png"} alt="Logo" className={s.TitleIcon} />
