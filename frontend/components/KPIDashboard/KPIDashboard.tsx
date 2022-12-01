f98ee0868509b7659e648655105514df02835fcd
maria besteman <maria.besteman@witteveenbos.com>
2022-12-01 12:26:06 +0000

    update icons and conditional rendering costs

--- frontend/components/KPIDashboard/KPIDashboard.tsx
+++ frontend/components/KPIDashboard/KPIDashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import KPIItem from "./KPIItem";
 import KPIRadioButtons from "./KPIRadiobuttons";
+
 type KPIDashboardProps = {
   data: Data;
   loading: boolean;
@@ -34,17 +35,16 @@ export default function KPIDashboard({ data, loading }: KPIDashboardProps) {
     }
   }
 
-  function CostKPIItemGenerator(level: string) {
+  function valueCosts(level: string) {
     let value = valueCheck(data[level].costs);
     if (level == "local") {
-      // divides by 1e9 because "miljard"
-      typeof value == "number" ? (value = value / 1e9) : (value = value);
-      return <KPIItem label="Betaalbaarheid" value={value} unit="mld.EUR/jaar"></KPIItem>;
-    } else {
       // divides by 1e3 because "k euro"
       typeof value == "number" ? (value = value / 1e3) : (value = value);
-      return <KPIItem label="Betaalbaarheid" value={value} unit="kEUR/jaar"></KPIItem>;
+    } else {
+      // divides by 1e9 because "mld euro"
+      typeof value == "number" ? (value = value / 1e9) : (value = value);
     }
+    return value;
   }
 
   return (
@@ -53,11 +53,26 @@ export default function KPIDashboard({ data, loading }: KPIDashboardProps) {
         <KPIRadioButtons updateValue={setLevel} loading={loading} />
       </div>
       <div className={`flex flex-row ${backgroundColor}`}>
-        <KPIItem label="Netbelasting" value={valueCheck(data[level].netload)} unit="%" />
-        {CostKPIItemGenerator(level)}
-        <KPIItem label="Duurzaamheid" value={valueCheck(data[level].sustainability)} unit="%" />
         <KPIItem
-          label="Zelfvoorzienendheid"
+          title="Netbelasting"
+          label="netload"
+          value={valueCheck(data[level].netload)}
+          unit="%"
+        />
+        <KPIItem
+          title="Betaalbaarheid"
+          label="costs"
+          unit={level === "local" ? "k.EUR/jaar" : "mld.EUR/jaar"}
+          value={valueCosts(level)}></KPIItem>
+        <KPIItem
+          title="Duurzaamheid"
+          label="sustainability"
+          value={valueCheck(data[level].sustainability)}
+          unit="%"
+        />
+        <KPIItem
+          title="Zelfvoorzienendheid"
+          label="self_sufficiency"
           value={valueCheck(data[level].self_sufficiency)}
           unit="%"></KPIItem>
       </div>
--- frontend/components/KPIDashboard/KPIItem.tsx
+++ frontend/components/KPIDashboard/KPIItem.tsx
@@ -6,6 +6,7 @@ import {
 } from "@heroicons/react/24/solid";
 
 type Props = {
+  title: string;
   label: string;
   value: string | number;
   unit: string;
@@ -18,13 +19,13 @@ const icon_map = {
   self_sufficiency: <MapPinIcon />,
 };
 
-export default function KPIItem({ label, value, unit }: Props) {
+export default function KPIItem({ title, label, value, unit }: Props) {
   return (
-    <div className="flex flex-col bg-transparant h-44 text-white justify-between items-center p-8 w-1/4">
-      <p>{label}</p>
-      <span className="w-[25%]">{icon_map[label]}</span>
+    <div className="flex flex-col bg-transparant text-white justify-between items-center p-6 w-1/4">
+      <span className="mb-2 text-xs md:text-base lg:text-lg">{title}</span>
+      <span className=" w-1/3 md:w-1/5 mb-4">{icon_map[label]}</span>
       <div className="flex flex-row items-center">
-        <span className="text-2xl font-bold mr-1">{value} </span>
+        <span className="text-xl md:text-3xl font-bold mr-1">{value} </span>
         <span className="text-base font-light">{unit}</span>
       </div>
     </div>
