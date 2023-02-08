235c11ce69bd468b22a6e9b4b70acf357da74ed6
ALFP <pascal.alferink@witteveenbos.com>
2023-02-08 13:39:37 +0000

    work-in-progress

--- frontend/pages/tiles-demo.tsx
+++ frontend/pages/tiles-demo.tsx
@@ -1,10 +1,34 @@
-import React from "react";
-import FloorPlan from "../components/FloorPlan/FloorPlan";
+import type { Props } from "@/containers/BasePage/BasePage";
+import { getPage } from "../api/wagtail";
 
-export default function Tiles() {
+import StaticPageWrap from "./_static-page-wrap";
+import FloorPlan from "@/components/FloorPlan/FloorPlan";
+
+export default function TilesDemo({
+  componentProps,
+}: {
+  componentName: string;
+  componentProps: Props;
+  children: any;
+}) {
+  const { navigation }: Props = componentProps;
+
   return (
-    <React.Fragment>
+    <StaticPageWrap
+      title={"dit is de titel"}
+      navigation={navigation}
+      componentProps={componentProps}>
       <FloorPlan />
-    </React.Fragment>
+    </StaticPageWrap>
   );
+}
+
+export async function getStaticProps({ params, preview, previewData }) {
+  console.log("get static props");
+  params = params || {};
+  let path = params.path || [];
+  path = path.join("/");
+
+  const { json: pageData } = await getPage(path);
+  return { props: pageData };
 }
