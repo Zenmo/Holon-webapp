e579457eda1fc1df8af13b8ed3238e8360383249
Gavin Broekhuis <broekhuisgavin@gmail.com>
2023-02-09 09:36:11 +0100

    Seperated KPI/InteractiveInput conditions

--- src/main/blocks/feedbackmodals.py
+++ src/main/blocks/feedbackmodals.py
@@ -4,8 +4,11 @@ from django.db import models
 from wagtail.fields import StreamBlock
 from wagtail.core import blocks
 
+from api.models import InteractiveInput
+from .holon_image_chooser import HolonImageChooserBlock
 
-class FeedbackModalCondition(blocks.StructBlock):
+
+class FeedbackModalKPICondition(blocks.StructBlock):
     class ParameterChoices(models.TextChoices):
         LOCALCOSTS = "localcosts", "Local Costs"
         LOCALNETLOAD = "localnetload", "Local Netload"
@@ -47,16 +50,55 @@ class FeedbackModalCondition(blocks.StructBlock):
     )
 
 
+class FeedbackModalInteractiveInputCondition(blocks.StructBlock):
+    def get_interactive_inputs():
+        return [(ii.pk, ii.__str__) for ii in InteractiveInput.objects.all()]
+
+    parameter = blocks.ChoiceBlock(choices=get_interactive_inputs)
+
+    operator = blocks.ChoiceBlock(
+        max_length=50,
+        choices=FeedbackModalKPICondition.OperatorChoices.choices,
+        default=FeedbackModalKPICondition.OperatorChoices.EQUAL,
+        required=True,
+        help_text=_("Set the operator of this condition"),
+    )
+
+    value = blocks.CharBlock(
+        max_length=255,
+        required=True,
+        help_text=_("Set the value of this condition to compare to"),
+    )
+
+
 class FeedbackModal(blocks.StructBlock):
+    class FeedbackModalThemes(models.TextChoices):
+        GREEN = "green", "Green"
+        ORANGE = "orange", "Orange"
+        RED = "red", "Red"
+
     modaltitle = blocks.CharBlock(
         max_length=255,
         required=False,
         help_text=_("Set the title of the feedback modal"),
     )
+    modaltext = blocks.TextBlock(
+        required=False,
+        help_text=_("Text of the modal"),
+    )
+    modaltheme = blocks.ChoiceBlock(
+        max_length=30,
+        choices=FeedbackModalThemes.choices,
+        default=FeedbackModalThemes.GREEN,
+        required=True,
+        help_text=_("Set the theme of this modal"),
+    )
+    image_selector = HolonImageChooserBlock()
 
     conditions = StreamBlock(
         [
-            ("conditions", FeedbackModalCondition()),
+            ("kpi_condition", FeedbackModalKPICondition()),
+            ("interactive_input_condition", FeedbackModalInteractiveInputCondition()),
         ],
         block_counts={},
         use_json_field=True,
