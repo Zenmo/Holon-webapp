a6080956012537b66467b364cacca3780e6e2f32
SCHS13 <sem.scholtes@witteveenbos.com>
2023-02-17 11:13:27 +0100

    Add datamodels and mapping for interactive input WIP

--- src/holon/models/factor.py
+++ src/holon/models/factor.py
@@ -1,5 +1,10 @@
+from django.apps import apps
+from django.core.exceptions import ValidationError
 from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+
+from holon.models.scenario_rule import ScenarioRule
+
 
 # Create your models here.
 class Factor(models.Model):
@@ -14,6 +19,8 @@ class Factor(models.Model):
     min_value = models.IntegerField()
     max_value = models.IntegerField()
 
+    rule = models.ForeignKey(ScenarioRule, on_delete=models.CASCADE, related_name="factors")
+
     panels = [
         FieldPanel("asset_attribute"),
         FieldPanel("min_value"),
@@ -23,8 +30,25 @@ class Factor(models.Model):
     class Meta:
         verbose_name = "Factors"
 
-    def __str__(self):
-        if self.asset is not None and self.grid_connection is not None:
-            return self.asset.type + "-" + self.grid_connection.type
-        else:
-            return self
+    # def __str__(self):
+    #     if self.asset is not None and self.grid_connection is not None:
+    #         return self.asset.type + "-" + self.grid_connection.type
+    #     else:
+    #         return self
+
+    def clean(self):
+        super().clean()
+
+        if not (
+            self.asset_attribute == "asset_attribute_not_supplied"
+            or self.asset_attribute in self.asset_attributes_values()
+        ):
+            raise ValidationError("Invalid value asset_attribute")
+
+    def asset_attributes_values(self):
+        model_type = (
+            self.rule.model_type if self.rule.model_subtype is None else self.rule.model_subtype
+        )
+        model = apps.get_model("holon", model_type)
+
+        return [field.name for field in model()._meta.get_fields() if not field.is_relation]
--- src/holon/models/policy.py
+++ src/holon/models/policy.py
@@ -1,7 +1,7 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
 from holon.models.scenario import Scenario
-from polymorphic.models import PolymorphicModel
 
 
 class Policy(PolymorphicModel):
@@ -9,4 +9,4 @@ class Policy(PolymorphicModel):
     value = models.CharField(max_length=255)
     unit = models.CharField(max_length=255, null=True, blank=True)
     comment = models.TextField()
-    payload = models.ForeignKey(Scenario, on_delete=models.CASCADE, related_name="gridconnections")
+    payload = models.ForeignKey(Scenario, on_delete=models.CASCADE)
