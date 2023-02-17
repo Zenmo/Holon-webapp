42047521ae65a6318af8694502936948b63f0ff6
SCHS13 <sem.scholtes@witteveenbos.com>
2023-02-17 11:42:35 +0100

    Add datamodels and mapping for interactive input WIP

---
+++ src/holon/models/filter.py
@@ -0,0 +1,95 @@
+from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.db.models import Q
+from polymorphic.models import PolymorphicModel
+
+
+class Filter(PolymorphicModel):
+    """Information on how to find the objects a scenario rule should be applied to"""
+
+    rule = models.ForeignKey("holon.ScenarioRule", on_delete=models.CASCADE, related_name="filters")
+
+
+class AttributeFilterComparator(models.TextChoices):
+    """Types of supported comparators"""
+
+    EQUAL = "EQUAL"
+    LESS_THAN = "LESS THAN"
+    GREATER_THAN = "GREATER THAN"
+    NOT_EQUAL = "NOT EQUAL"
+
+
+class AttributeFilter(Filter):
+    """Filter on attribute"""
+
+    model_attribute = models.CharField(max_length=255, null=True)
+    comparator = models.CharField(max_length=255, choices=AttributeFilterComparator.choices)
+    value = models.IntegerField()
+
+    filter(Q(naam=5))
+
+    def getQ(self):
+        if self.comparator == AttributeFilterComparator.EQUAL:
+            return Q(**{self.model_attribute: self.value})
+        if self.comparator == AttributeFilterComparator.LESS_THAN:
+            return Q(**{f"{self.model_attribute}__lt": self.value})
+        if self.comparator == AttributeFilterComparator.GREATER_THAN:
+            return Q(**{f"{self.model_attribute}__gt": self.value})
+        if self.comparator == AttributeFilterComparator.NOT_EQUAL:
+            return ~Q(**{self.model_attribute: self.value})
+
+
+class RelationAttributeFilter(AttributeFilter):
+    """Filter on attribute for parent object"""
+
+    relation_field = models.CharField(max_length=255)  # bijv gridconnection
+    relation_field_subtype = models.CharField(max_length=255, blank=True)  # bijv household
+
+    def relation_field_values(self):
+        model_type = (
+            self.rule.model_type if self.rule.model_subtype is None else self.rule.model_subtype
+        )
+        model = apps.get_model("holon", model_type)
+
+        return [field.name for field in model()._meta.get_fields() if field.is_relation]
+
+    def relation_field_subtype_values(self) -> list[str]:
+        def all_subclasses(cls) -> list[models.Model]:
+            return set(cls.__subclasses__()).union(
+                [s for c in cls.__subclasses__() for s in all_subclasses(c)]
+            )
+
+        model = apps.get_model(self.rule.model_type)
+        related_model = model()._meta.get_field(self.relation_field).related_model
+
+        return [subclass.__name__ for subclass in all_subclasses(related_model)]
+
+    def getQ(self) -> Q:
+        relation_field_q = Q()
+        relation_field_subtype = Q()
+
+        if self.comparator == AttributeFilterComparator.EQ:
+            relation_field_q = Q(**{f"{self.relation_field}__{self.model_attribute}": self.value})
+        elif self.comparator == AttributeFilterComparator.LT:
+            relation_field_q = Q(
+                **{f"{self.relation_field}__{self.model_attribute}__lt": self.value}
+            )
+        elif self.comparator == AttributeFilterComparator.GT:
+            relation_field_q = Q(
+                **{f"{self.relation_field}__{self.model_attribute}__gt": self.value}
+            )
+        elif self.comparator == AttributeFilterComparator.NE:
+            relation_field_q = ~Q(**{f"{self.relation_field}__{self.model_attribute}": self.value})
+
+        if self.relation_field_subtype:
+            relation_subtype = apps.get_model("holon", self.relation_field_subtype)
+            relation_field_subtype = Q(
+                **{
+                    f"{self.relation_field}__polymorphic_ctype": ContentType.objects.get_for_model(
+                        relation_subtype
+                    )
+                }
+            )
+
+        return relation_field_q & relation_field_subtype
---
+++ src/holon/models/scenario_rule.py
@@ -0,0 +1,51 @@
+from django.apps import apps
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from holon.models.interactive_element import InteractiveElement
+
+
+class ModelType(models.TextChoices):
+    """Types of models"""
+
+    ACTOR = "Actor"
+    ENERGYASSET = "EnergyAsset"
+    GRIDNODE = "GridNode"
+    GRIDCONNECTION = "GridConnection"
+    POLICY = "Policy"
+
+
+class ScenarioRule(models.Model):
+    """A rule that finds a selection of objects and updates an attribute according to user input"""
+
+    interactive_element = models.ForeignKey(
+        InteractiveElement, on_delete=models.CASCADE, related_name="rules"
+    )
+    model_type = models.CharField(
+        max_length=255, choices=ModelType.choices
+    )  # bijv gridconnection of asset
+    model_subtype = models.CharField(
+        max_length=255, null=True, blank=True
+    )  # bijv industry terrain of photovoltaic
+
+    class Meta:
+        verbose_name = "ScenarioRule"
+
+    def clean(self):
+        super().clean()
+
+        if (
+            self.model_subtype is not None
+            and self.model_subtype != ""
+            and self.model_subtype not in self.model_subtype_options()
+        ):
+            raise ValidationError("Invalid value model_subtype")
+
+    def model_subtype_options(self):
+        def all_subclasses(cls):
+            return set(cls.__subclasses__()).union(
+                [s for c in cls.__subclasses__() for s in all_subclasses(c)]
+            )
+
+        model_type_class = apps.get_model("holon", self.model_type)
+        return [subclass.__name__ for subclass in all_subclasses(model_type_class)]
