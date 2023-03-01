1022f6e90b1799102182b561a3df90e7e6acc149
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-01 08:33:05 +0000

    adds key value pairs at ETM config level

--- src/holon/models/config.py
+++ src/holon/models/config.py
@@ -122,6 +122,12 @@ class QueryAndConvertConfig(ClusterableModel):
             label="ETM query/input statement",
             min_num=1,
         ),
+        InlinePanel(
+            "key_value_pair_collection",
+            heading="Define local key-value pairs here",
+            label="Local variables",
+            max_num=1,
+        ),
     ]
 
     def __str__(self):
@@ -131,6 +137,36 @@ class QueryAndConvertConfig(ClusterableModel):
             return f"Kostenmodule configuratie ({self.name})"
 
         raise NotImplementedError(f"__str__ is not implemented for {self.module}")
+
+
+class KeyValuePairCollection(ClusterableModel):
+    related_config = ParentalKey(QueryAndConvertConfig, related_name="key_value_pair_collection")
+
+    panels = [
+        InlinePanel(
+            "float_key_value_pair",
+            heading="Define float type key-value pair",
+            label="Float key-value pair",
+        )
+    ]
+
+    class Meta:
+        verbose_name = "ETM module configuratie variabelen"
+
+    def __str__(self):
+        return f"ETM module configuratie variabelen"
+
+
+class FloatKeyValuePair(models.Model):
+    related_key_value_collection = ParentalKey(
+        KeyValuePairCollection, related_name="float_key_value_pair"
+    )
+
+    key = models.CharField(max_length=255, null=False, blank=False)
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"({self.key}: {self.value})"
 
 
 class EndPoint(models.TextChoices):
