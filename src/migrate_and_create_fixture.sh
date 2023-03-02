72945f33099aa6d08b11d212cced450394f6e85d
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-02 18:03:23 +0000

    utility script and fixture

---
+++ src/migrate_and_create_fixture.sh
@@ -0,0 +1,12 @@
+# Just a little convience script for the lazy (and gives some certainty with regards to the fixture)
+set -eux
+fixture='latest-datamodel-mvp-fixture.json'
+
+# Migrate and apply
+python manage.py makemigrations
+python manage.py migrate
+
+# make fixture
+python manage.py dumpdata --natural-primary --natural-foreign holon > $
+# load fixture just to be sure
+python manage.py loaddata $fixture
