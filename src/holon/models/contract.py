98e544224a83cf0afb23af271b5ddd126c6c4a3a
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-09 13:26:40 +0000

    update clean functions

--- src/holon/models/contract.py
+++ src/holon/models/contract.py
@@ -22,6 +22,9 @@ class EnergyCarrier(models.TextChoices):
 
 
 class Contract(PolymorphicModel):
+    contractType = models.CharField(
+        max_length=255, choices=ContractType.choices, default=ContractType.DELIVERY
+    )
     contractScope = models.ForeignKey(Actor, on_delete=models.PROTECT)
     energyCarrier = models.CharField(
         max_length=255, choices=EnergyCarrier.choices, default=EnergyCarrier.ELECTRICITY
@@ -39,6 +42,11 @@ class Contract(PolymorphicModel):
     def __str__(self):
         return f"c{self.id} - {self.contractType.lower()}"
 
+    def clean(self):
+        return ValidationError(
+            "Should not be implemented at top level! Use a specific class for this case."
+        )
+
 
 class DeliveryContractType(models.TextChoices):
     FIXED = "ELECTRICITY_FIXED"
@@ -46,16 +54,15 @@ class DeliveryContractType(models.TextChoices):
 
 
 class DeliveryContract(Contract):
-
-    def clean(self) -> None:
-        if self.contractType != ContractType.DELIVERY:
-            raise ValidationError(f"ContractType should be {Delivery} for {self.__name__}")
-
-        return super().clean()
-
     deliveryContractType = models.CharField(max_length=255, choices=DeliveryContractType.choices)
     deliveryPrice_eurpkWh = models.FloatField()
     feedinPrice_eurpkWh = models.FloatField()
+
+    def clean(self) -> None:
+        if self.contractType != ContractType.DELIVERY:
+            raise ValidationError(f"ContractType should be 'Delivery' for {self.__name__}")
+
+        return super().clean()
 
 
 class ConnectionContractType(models.TextChoices):
@@ -64,19 +71,18 @@ class ConnectionContractType(models.TextChoices):
 
 
 class ConnectionContract(Contract):
-
-    def clean(self) -> None:
-        if self.contractType != ContractType.CONNECTION:
-            raise ValidationError(f"ContractType should be {Connection} for {self.__name__}")
-
-        return super().clean()
-
     connectionContractType = models.CharField(
         max_length=255, choices=ConnectionContractType.choices
     )
     nfATO_capacity_kW = models.FloatField()
     nfATO_starttime_h = models.FloatField()
     nfATO_endtime_h = models.FloatField()
+
+    def clean(self) -> None:
+        if self.contractType != ContractType.CONNECTION:
+            raise ValidationError(f"ContractType should be 'Connection' for {self.__name__}")
+
+        return super().clean()
 
 
 class TaxContractType(models.TextChoices):
@@ -85,17 +91,16 @@ class TaxContractType(models.TextChoices):
 
 
 class TaxContract(Contract):
-
-    def clean(self) -> None:
-        if self.contractType != ContractType.TAX:
-            raise ValidationError(f"ContractType should be {Tax} for {self.__name__}")
-
-        return super().clean()
-
     taxContractType = models.CharField(max_length=255, choices=TaxContractType.choices)
     taxDelivery_eurpkWh = models.FloatField()
     taxFeedin_eurpkWh = models.FloatField()
     proportionalTax_pct = models.FloatField()
+
+    def clean(self) -> None:
+        if self.contractType != ContractType.TAX:
+            raise ValidationError(f"ContractType should be 'Tax' for {self.__name__}")
+
+        return super().clean()
 
 
 class TransportContractType(models.TextChoices):
@@ -105,13 +110,12 @@ class TransportContractType(models.TextChoices):
 
 
 class TransportContract(Contract):
-
-    def clean(self) -> None:
-        if self.contractType != ContractType.TRANSPORT:
-            raise ValidationError(f"ContractType should be {Transport} for {self.__name__}")
-
-        return super().clean()
-
     transportContractType = models.CharField(max_length=255, choices=TransportContractType.choices)
     bandwidthTreshold_kW = models.FloatField()
     bandwidthTariff_eurpkWh = models.FloatField()
+
+    def clean(self) -> None:
+        if self.contractType != ContractType.TRANSPORT:
+            raise ValidationError(f"ContractType should be 'Transport' for {self.__name__}")
+
+        return super().clean()
