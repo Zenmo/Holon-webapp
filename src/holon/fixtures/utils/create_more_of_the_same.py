8257d7801018efca0a193a24241f316686634ea3
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-10 13:09:52 +0000

    format with black

--- src/holon/fixtures/utils/create_more_of_the_same.py
+++ src/holon/fixtures/utils/create_more_of_the_same.py
@@ -1,13 +1,9 @@
 import jinja2
 
-with open("dieseltruck.json.j2", 'r') as infile:
+with open("dieseltruck.json.j2", "r") as infile:
     template = jinja2.Template(infile.read())
 
 i_0 = 1000
 for i in range(20):
     i = i_0 + i
-    print(template.render(
-        pk=i,
-        gridconnection=2
-    )
-    )
+    print(template.render(pk=i, gridconnection=2))
