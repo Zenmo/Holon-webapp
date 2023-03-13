f7b9a06958d2dbdb039783bcb81870ebfdb8f13d
Gavin Broekhuis <broekhuisgavin@gmail.com>
2023-03-13 10:23:44 +0000

    Add colors also to the challengemode page

--- frontend/containers/ChallengeModePage/ChallengeModePage.tsx
+++ frontend/containers/ChallengeModePage/ChallengeModePage.tsx
@@ -2,7 +2,7 @@ import { basePageWrap } from "@/containers/BasePage";
 import styles from "./ChallengeModePage.module.css";
 import React from "react";
 
-import { PageProps, SectionVariant, TextAndMediaVariant } from "../types";
+import { PageProps, SectionVariant, TextAndMediaVariant, Graphcolor } from "../types";
 import ContentBlocks from "@/components/Blocks/ContentBlocks";
 import { FeedbackModal } from "@/components/Blocks/ChallengeFeedbackModal/types";
 
@@ -13,13 +13,20 @@ export type Feedbackmodals = [FeedbackModal];
 const ChallengeModePage = ({
   storyline,
   feedbackmodals,
+  graphcolors,
 }: {
   storyline: Storyline[];
   feedbackmodals: Feedbackmodals[];
+  graphcolors?: Graphcolor[];
 }) => {
   return (
     <div className={styles["ChallengeModePage"]}>
-      <ContentBlocks content={storyline} feedbackmodals={feedbackmodals} pagetype="Challenge" />
+      <ContentBlocks
+        content={storyline}
+        feedbackmodals={feedbackmodals}
+        graphcolors={graphcolors ?? []}
+        pagetype="Challenge"
+      />
     </div>
   );
 };
