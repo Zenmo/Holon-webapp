2ae5bf60e458a7202752583eadd5861bc6bf4840
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-14 19:02:49 +0000

    some more work on parsing for ease

--- src/holon/scripts/query_and_convert.py
+++ src/holon/scripts/query_and_convert.py
@@ -3,11 +3,13 @@ from holon.models.config import (
     QueryAndConvertConfig,
     KeyValuePairCollection,
     FloatKeyValuePair,
+    AnyLogicConversion,
+    DatamodelConversion,
+    StaticConversion,
+    ETMConversion,
     ETMQuery,
 )
-from django.db import models
-
-models.prefetch_related_objects
+from typing import List
 
 
 class QConfig:
@@ -36,6 +38,55 @@ class QConfig:
             pass
 
 
+class Query:
+    def __init__(self, query: ETMQuery) -> None:
+        self.internal_key = query.internal_key
+        self.data_type = query.data_type
+        self.endpoint = query.endpoint
+        self.etm_key = query.etm_key
+
+    @property
+    def convert_with(self) -> List[dict]:
+        try:
+            return self._convert_with
+        except AttributeError:
+            return list()
+
+    @convert_with.setter
+    def convert_with(self, query: ETMQuery) -> None:
+        self._convert_with = []
+        conversions: List[ETMQuery] = [
+            *query.static_conversion_step.all(),
+            *query.etm_conversion_step.all(),
+            *query.datamodel_conversion_step.all(),
+            *query.al_conversion_step.all(),
+        ]
+
+        for c in conversions:
+            if isinstance(c, StaticConversion):
+                self._convert_with.append(self.set_static(c))
+            elif isinstance(c, ETMConversion):
+                self._convert_with.append(self.set_etm(c))
+            elif isinstance(c, AnyLogicConversion):
+                self._convert_with.append(self.set_anylogic(c))
+            elif isinstance(c, DatamodelConversion):
+                self._convert_with.append(self.set_datamodel(c))
+            else:
+                raise NotImplementedError(f"Conversion of type {c.__name__} is not implemented!")
+
+    def set_static(self, conversion: StaticConversion):
+        return conversion
+
+    def set_etm(self, conversion: ETMConversion):
+        return conversion
+
+    def set_anylogic(self, conversion: AnyLogicConversion):
+        return conversion
+
+    def set_datamodel(self, conversion: DatamodelConversion):
+        return conversion
+
+
 def run():
     scenario = Scenario.objects.get(id=1)
     configs: list[QueryAndConvertConfig] = scenario.query_and_convert_config.all()
@@ -46,7 +97,4 @@ def run():
         for query in c.etm_query.all():
             query: ETMQuery = query
 
-            query.internal_key
-            query.data_type
-            query.endpoint
-            query.etm_key
+            print()
