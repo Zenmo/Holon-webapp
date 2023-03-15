bcc4a0a87cfffaaba0c5b2e012b1bd4aff546c9c
= <=>
2023-03-15 08:21:03 +0000

    Add test to exclusion list in apps

--- src/holon/apps.py
+++ src/holon/apps.py
@@ -8,7 +8,7 @@ class HolonConfig(AppConfig):
     name = "holon"
 
     def ready(self):
-        if "makemigrations" in sys.argv or "migrate" in sys.argv:
+        if "makemigrations" in sys.argv or "migrate" in sys.argv or "test" in sys.argv:
             from .models.rule_actions.rule_action_balance_group import RuleActionBalanceGroup
 
             RuleActionBalanceGroup.selected_model_type_name.field.choices = None
