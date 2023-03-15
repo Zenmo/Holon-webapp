8c9affcc53ebdf567cd7d6a1cc8c2ab1252e11fa
WBAD\TAVM <matthias.tavasszy@witteveenbos.com>
2023-03-15 17:32:45 +0100

    filter aggregation setup, WIP

---
+++ src/holon/models/filter_aggregation.py
@@ -0,0 +1,36 @@
+from django.db.models import Q
+from django.db.models.query import QuerySet
+
+from holon.models.filter import Filter
+
+
+class FilterAggregation:
+    def get_filters_object_count(filters: list[Filter]) -> int:
+        """Get the number of objects in the combined filter resutls"""
+        filtered_queryset = FilterAggregation.get_queryset_from_filters(filters)
+        return filtered_queryset.count()
+
+    def get_filters_attribute_sum(filters: list[Filter], attribute_name: str) -> float:
+        """Return the sum of a specific attribute of all objects in a queryset"""
+
+        filtered_queryset = FilterAggregation.get_queryset_from_filters(filters)
+
+        attr_sum = 0
+
+        for filtered_object in filtered_queryset:
+            try:
+                value = getattr(filtered_object, attribute_name)
+                attr_sum += value
+            except Exception as e:
+                print(f"Something went wrong, let's act as if nothing happend and keep going ({e})")
+
+        return attr_sum
+
+    def get_queryset_from_filters(filters: list[Filter]) -> QuerySet:
+        queryset_filter = Q()
+
+        # filter: Filter
+        for filter in filters:
+            queryset_filter &= filter.get_q()
+
+        return base_queryset.filter(queryset_filter)
