370742024eb92ce48927a76db5a1c2376f071f78
= <=>
2023-03-15 08:13:56 +0000

    Fixes dynamic choices in migration

--- src/holon/apps.py
+++ src/holon/apps.py
@@ -1,6 +1,14 @@
+import sys
+
 from django.apps import AppConfig
 
 
 class HolonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "holon"
+
+    def ready(self):
+        if "makemigrations" in sys.argv or "migrate" in sys.argv:
+            from .models.rule_actions.rule_action_balance_group import RuleActionBalanceGroup
+
+            RuleActionBalanceGroup.selected_model_type_name.field.choices = None
