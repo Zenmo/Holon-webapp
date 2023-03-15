ba0511410681f006fb0dd871276ce8bf10f973dd
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-15 21:01:37 +0000

    working cost benefit

--- src/holon/services/costbenefit.py
+++ src/holon/services/costbenefit.py
@@ -11,13 +11,22 @@ class CostBenedict:
         "Netbeheerder": "balanceElectricityTransport_eur",
         "Overheid": "balanceElectricityTax_eur",
     }
+    # not existing key; just for fun (only the slash is meaningful for later use)
+    NEK = "h4ck3rm4n69_was_here/"
 
     def __init__(self, actors: list) -> None:
         self.actors = actors
-        self.WANT_TO_KNOW
+
+        self.want_to_know = self.WANT_TO_KNOW
+
+        # update the want to know with the holon actor group name (nice name) and a key
+        # that we are very sure of that it does not exist ;)
+        for actor in actors:
+            if "hol" in actor["actorID"]:
+                self.want_to_know.update({actor["group"]: self.NEK + actor["actorID"]})
 
     def determine_group_costs(self):
-        return self.determine_cost(groupby="group", want_to_know=self.WANT_TO_KNOW)
+        return self.determine_cost(groupby="group", want_to_know=self.want_to_know)
 
     def determine_cost(self, groupby: str, want_to_know: dict):
         result = {}
@@ -46,11 +55,32 @@ class CostBenedict:
         else:
             return False
 
-    @staticmethod
-    def sum_values(group: str, group_members: list, want_to_know: dict) -> dict:
-        return {
-            group: {
-                wkey: sum([float(member[wvalue]) for member in group_members if member[""]])
-                for wkey, wvalue in want_to_know.items()
-            }
-        }
+    def sum_values(self, group: str, group_members: list, want_to_know: dict) -> dict:
+        """based on the configuration sum differently"""
+        # TODO assumes based on parentholon property (acutally quite a nice solution)
+
+        group_sum = {key: 0 for key in want_to_know.keys()}
+
+        for member in group_members:
+            if member["parentHolon"] is not None:
+                for key in group_sum.keys():
+                    try:
+                        holon_id = want_to_know[key].split("/")[1]
+                        if member["parentHolon"] == holon_id:
+                            group_sum[key] += sum(
+                                [
+                                    float(member[value]) if self.NEK not in value else 0
+                                    for value in want_to_know.values()
+                                ]
+                            )
+                    except IndexError:
+                        pass
+
+            else:
+                for key in group_sum.keys():
+                    try:
+                        group_sum[key] += float(member[want_to_know[key]])
+                    except KeyError:
+                        pass
+
+        return {group: group_sum}
