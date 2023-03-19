1cd89ba5736e8386927cb1755bf52e1081da94c7
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-03-19 11:47:10 +0000

    redo catch error

--- src/holon/views/holon.py
+++ src/holon/views/holon.py
@@ -2,14 +2,13 @@ from django.apps import apps
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from holon.models import rule_mapping, Scenario
+from holon.models import Scenario, rule_mapping
 from holon.models.scenario_rule import ModelType
-from holon.services.cloudclient import CloudClient
-from holon.services import CostBenedict
-from holon.services.data import Results
-
-from holon.serializers import HolonRequestSerializer
 from holon.models.util import all_subclasses
+from holon.serializers import HolonRequestSerializer
+from holon.services import CostBenedict
+from holon.services.cloudclient import CloudClient
+from holon.services.data import Results
 
 DUMMY_UPSCALE = {"sustainability": 42, "self_sufficiency": 42, "netload": 42, "costs": 42}
 DUMMY_COST = 42
@@ -21,42 +20,44 @@ class HolonV2Service(generics.CreateAPIView):
     def post(self, request):
         serializer = HolonRequestSerializer(data=request.data)
 
-        # try:
-        if serializer.is_valid():
-            data = serializer.validated_data
+        try:
+            if serializer.is_valid():
+                data = serializer.validated_data
 
-            scenario = rule_mapping.get_scenario_and_apply_rules(
-                data["scenario"].id, data["interactive_elements"]
-            )
+                scenario = rule_mapping.get_scenario_and_apply_rules(
+                    data["scenario"].id, data["interactive_elements"]
+                )
 
-            # TODO hand over the original_scenario for configs and the edited scenario for datamodel
-            original_scenario = Scenario.objects.get(id=data["scenario"].id)
-            cc = CloudClient(original_scenario)
-            cc.run()
+                # TODO hand over the original_scenario for configs and the edited scenario for datamodel
+                original_scenario = Scenario.objects.get(id=data["scenario"].id)
+                cc = CloudClient(original_scenario)
+                cc.run()
 
-            cost_benefit_results = CostBenedict(actors=cc.outputs["actors"]).determine_group_costs()
+                cost_benefit_results = CostBenedict(
+                    actors=cc.outputs["actors"]
+                ).determine_group_costs()
 
-            results = Results(
-                scenario=scenario,
-                anylogic_outcomes=cc.outputs,
-                inter_upscaling_outcomes=DUMMY_UPSCALE,
-                nat_upscaling_outcomes=DUMMY_UPSCALE,
-                cost_outcome=DUMMY_COST,
-                cost_benefit_detail=cost_benefit_results,  # TODO: twice the same!
-                cost_benefit_overview=cost_benefit_results,  # TODO: twice the same!
-            )
+                results = Results(
+                    scenario=scenario,
+                    anylogic_outcomes=cc.outputs,
+                    inter_upscaling_outcomes=DUMMY_UPSCALE,
+                    nat_upscaling_outcomes=DUMMY_UPSCALE,
+                    cost_outcome=DUMMY_COST,
+                    cost_benefit_detail=cost_benefit_results,  # TODO: twice the same!
+                    cost_benefit_overview=cost_benefit_results,  # TODO: twice the same!
+                )
 
-            # Delete duplicated scenario
-            scenario.delete()
-            return Response(
-                results.to_dict(),
-                status=status.HTTP_200_OK,
-            )
+                # Delete duplicated scenario
+                scenario.delete()
+                return Response(
+                    results.to_dict(),
+                    status=status.HTTP_200_OK,
+                )
 
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # except Exception as e:
-        #     return Response(f"Something went wrong: {e}", status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(f"Something went wrong: {e}", status=status.HTTP_400_BAD_REQUEST)
 
 
 class HolonCMSLogic(generics.RetrieveAPIView):
