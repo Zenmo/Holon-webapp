ea751dde703eec71ff12551883ab6544fe6989cb
ALFP <pascal.alferink@witteveenbos.com>
2023-04-17 08:10:09 +0000

    update holarchy feedback image conditions

--- frontend/components/Blocks/HolarchyFeedbackImage/HolarchyFeedbackImage.tsx
+++ frontend/components/Blocks/HolarchyFeedbackImage/HolarchyFeedbackImage.tsx
@@ -44,7 +44,7 @@ export default function HolarchyFeedbackImage({ content, holarchyfeedbackimages 
           for (const conditionItem of feedbackimage.value.conditions) {
             //inputvalue is the vaule of the assessed validator
             const inputvalue = content?.find(
-              content => content.value?.id == parseFloat(conditionItem.value.parameter)
+              content => content.value.id == parseFloat(conditionItem.value.parameter)
             )?.currentValue;
 
             const conditionValue = parseFloat(conditionItem.value.value);
@@ -60,12 +60,12 @@ export default function HolarchyFeedbackImage({ content, holarchyfeedbackimages 
               return false;
             } else if (
               conditionItem.value.operator == "equal" &&
-              inputvalue != conditionItem.value.value
+              inputvalue.toLowerCase() != conditionItem.value.value.toLowerCase()
             ) {
               return false;
             } else if (
               conditionItem.value.operator == "notequal" &&
-              inputvalue == conditionItem.value.value
+              inputvalue.toLowerCase() == conditionItem.value.value.toLowerCase()
             ) {
               return false;
             } else if (conditionItem.value.operator == "lower" && inputvalue >= conditionValue) {
