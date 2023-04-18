7c6f678119cc32cff87d01b0abfe7a0ae7004418
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-04-18 14:52:14 +0000

    fix formatting

--- src/holon/services/query_and_convert.py
+++ src/holon/services/query_and_convert.py
@@ -3,10 +3,16 @@ from typing import List
 import etm_service
 
 from holon.models import DatamodelQueryRule, Scenario
-from holon.models.config import (AnyLogicConversion, DatamodelConversion,
-                                 ETMConversion, ETMQuery, FloatKeyValuePair,
-                                 KeyValuePairCollection, QueryAndConvertConfig,
-                                 StaticConversion)
+from holon.models.config import (
+    AnyLogicConversion,
+    DatamodelConversion,
+    ETMConversion,
+    ETMQuery,
+    FloatKeyValuePair,
+    KeyValuePairCollection,
+    QueryAndConvertConfig,
+    StaticConversion,
+)
 
 
 def pprint(msg: str):
