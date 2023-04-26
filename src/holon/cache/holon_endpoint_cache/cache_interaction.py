c0c1542208b6bc0b9ab8048f3ea1ed63f181d754
WBAD\TAVM <matthias.tavasszy@witteveenbos.com>
2023-04-26 21:33:05 +0200

    add error catching for cache interaction

--- src/holon/cache/holon_endpoint_cache/cache_interaction.py
+++ src/holon/cache/holon_endpoint_cache/cache_interaction.py
@@ -3,27 +3,34 @@ import json
 from django.conf import settings
 
 from holon.cache.holon_endpoint_cache.config import Config
+from holon.utils.logging import HolonLogger
 
 
 def set(key: str, result: dict) -> None:
     """Set a record int the holon_cache database"""
 
-    result = json.dumps(result)
-    caches[Config.cache_name].set(key, result, timeout=settings.HOLON_CACHING_TIMEOUT)
-    Config.logger.log_print(f"Holon cache write: {key}")
+    try:
+        result = json.dumps(result)
+        caches[Config.cache_name].set(key, result, timeout=settings.HOLON_CACHING_TIMEOUT)
+        Config.logger.log_print(f"Holon cache write: {key}")
+    except Exception as e:
+        Config.logger.log_print(f"Cache write failed for key {key}:\n{e}")
 
 
 def get(key: str) -> dict:
     """Get a record from the holon_cache database"""
 
-    value = caches[Config.cache_name].get(key)
+    try:
+        value = caches[Config.cache_name].get(key)
 
-    if not value:
-        Config.logger.log_print(f"Holon cache miss. key: {key}")
-    else:
-        value = json.loads(value)
+        if not value:
+            Config.logger.log_print(f"Holon cache miss. key: {key}")
+        else:
+            value = json.loads(value)
 
-    return value
+        return value
+    except Exception as e:
+        Config.logger.log_print(f"Cache read failed for key {key}:\n{e}")
 
 
 def clear_scenario(scenario_id: int):
