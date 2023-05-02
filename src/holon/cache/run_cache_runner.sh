0b86b27e0b0cbea2e00652b9268670dcc6237aaf
thesethtruth <s.w.p.vanwieringen@gmail.com>
2023-05-02 11:22:25 +0000

    make util script parse args

--- src/holon/cache/run_cache_runner.sh
+++ src/holon/cache/run_cache_runner.sh
@@ -1,5 +1,43 @@
+#! /bin/bash -
+PROGNAME=$0
+
+usage() {
+  cat << EOF >&2
+Usage: $PROGNAME [-i <scenario_ids>] [-c <include_challenge>] [-s <include_storyline>]
+
+ -i <scenario_ids> (comma seperated ints): ...
+ -c <include_challenge> (bool in Python format): ...
+ -s <include_storyline> (bool in Python format): ...
+EOF
+  exit 1
+}
+
+scenario_ids=() challenge="True" storyline="True"
+while getopts i:s:c: o; do
+  case $o in
+    (i) scenario_ids+=$OPTARG;;
+    (c) challenge=$OPTARG;;
+    (s) storyline=$OPTARG;;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+
+echo "Running:
+    from holon.cache.cache_runner import cache_runner
+    cache_runner.update_cache(
+        scenario_ids=[$scenario_ids],
+        include_storyline=$storyline,
+        include_challenge=$challenge
+    )
+"
+
 python manage.py createcachetable
 python manage.py shell <<EOF 
 from holon.cache.cache_runner import cache_runner
-cache_runner.update_cache(scenario_ids=[1, 321001])
+cache_runner.update_cache(
+    scenario_ids=[$scenario_ids],
+    include_storyline=$storyline,
+    include_challenge=$challenge
+)
 EOF
