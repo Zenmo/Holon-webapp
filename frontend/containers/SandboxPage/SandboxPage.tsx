d2c3ee5a6059f04fdd21a1c5a545c50a9a2dc7d9
Gavin Broekhuis <broekhuisgavin@gmail.com>
2023-06-05 15:09:31 +0000

    Make the Sandbox page updated with the challengemode page again

---
+++ frontend/containers/SandboxPage/SandboxPage.tsx
@@ -0,0 +1,38 @@
+import { basePageWrap } from "@/containers/BasePage";
+import styles from "./SandboxPage.module.css";
+
+import { FeedbackModal } from "@/components/Blocks/ChallengeFeedbackModal/types";
+import ContentBlocks from "@/components/Blocks/ContentBlocks";
+import { ScenarioContext } from "context/ScenarioContext";
+import { Graphcolor, PageProps, SectionVariant, TextAndMediaVariant } from "../types";
+
+type Storyline = PageProps<SectionVariant | TextAndMediaVariant>;
+
+export type Feedbackmodals = [FeedbackModal];
+
+const SandboxPage = ({
+  storyline,
+  scenario,
+  feedbackmodals,
+  graphcolors,
+}: {
+  storyline: Storyline[];
+  scenario: number;
+  feedbackmodals: Feedbackmodals[];
+  graphcolors: Graphcolor[];
+}) => {
+  return (
+    <div className={styles["ChallengeModePage"]}>
+      <ScenarioContext.Provider value={scenario}>
+        <ContentBlocks
+          content={storyline}
+          graphcolors={graphcolors ?? []}
+          feedbackmodals={feedbackmodals}
+          pagetype="Challenge"
+        />
+      </ScenarioContext.Provider>
+    </div>
+  );
+};
+
+export default basePageWrap(SandboxPage);
