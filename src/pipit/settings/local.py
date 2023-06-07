529be3011aeb130c4cf25106913db2d481419475
stam4 <mattijs.stam@witteveenbos.com>
2023-06-07 08:59:29 +0000

    Removes settings from local.py

--- src/pipit/settings/local.py
+++ src/pipit/settings/local.py
@@ -7,16 +7,7 @@ from pipit.settings.base import *  # NOQA
 MIDDLEWARE += [
     "opencensus.ext.django.middleware.OpencensusMiddleware",
 ]
-ALLOWED_HOSTS = ["localhost", "*.azurewebsites.net"]
-APPLICATION_INSIGHTS_CONNECTION_STRING = "InstrumentationKey=bb6563f2-f484-443e-9a27-09168b466be3;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
-OPENCENSUS = {
-    "TRACE": {
-        "SAMPLER": "opencensus.trace.samplers.ProbabilitySampler(rate=1)",
-        "EXPORTER": f"""opencensus.ext.azure.trace_exporter.AzureExporter(
-                  connection_string='${APPLICATION_INSIGHTS_CONNECTION_STRING}',
-        )""",
-    }
-}
+
 WAGTAILADMIN_BASE_URL = "http://localhost:8000"
 
 MEDIA_ROOT = "/media/"
