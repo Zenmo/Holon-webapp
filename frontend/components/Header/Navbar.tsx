4b0746f11f65880245d7e4a78cffefd170c2bb53
Gavin Broekhuis <gavin.broekhuis@witteveenbos.com>
2023-06-12 10:05:21 +0200

    Removed login button from header

--- frontend/components/Header/Navbar.tsx
+++ frontend/components/Header/Navbar.tsx
@@ -1,13 +1,9 @@
+import { NavItem } from "@/api/types";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { NavItem } from "@/api/types";
-import TokenService from "@/services/token";
 
 export default function Navbar({
   items,
-  loggedIn,
-  nameUser,
-  mutateUser,
 }: {
   items: NavItem[];
   loggedIn: boolean;
@@ -15,27 +11,6 @@ export default function Navbar({
   mutateUser: () => void;
 }) {
   const router = useRouter();
-
-  function handleLogOut() {
-    mutateUser(TokenService.removeAccessToken());
-    router.push("/inloggen");
-  }
-
-  const statusButton = (status: boolean) => {
-    if (status === true) {
-      return (
-        <button onClick={handleLogOut} className="buttonDark">
-          Uitloggen
-        </button>
-      );
-    } else {
-      return (
-        <Link href="/inloggen">
-          <a className="buttonDark">Inloggen</a>
-        </Link>
-      );
-    }
-  };
 
   return (
     <ul className="flex flex-col md:flex-row md:space-x-8 md:mt-0">
@@ -54,24 +29,6 @@ export default function Navbar({
           </li>
         );
       })}
-
-      {loggedIn && nameUser ? (
-        <li>
-          <Link href="/profiel">
-            <a
-              className={`w-full cursor-pointer px-2 py-3 md:py-2 text-xl text-holon-blue-900 dark:text-white hover:underline hover:decoration-gray-300 hover:decoration-2 hover:underline-offset-4 sm:p-0 ${
-                router.asPath === `/profiel` &&
-                "underline decoration-holon-blue-900 decoration-2 underline-offset-4"
-              }`}>
-              {nameUser}
-            </a>
-          </Link>
-        </li>
-      ) : (
-        ""
-      )}
-
-      <li>{statusButton(loggedIn)}</li>
     </ul>
   );
 }
