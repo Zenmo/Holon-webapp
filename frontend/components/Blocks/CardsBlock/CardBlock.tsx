974bec68005ce2794febcae0b4fe33beae979342
Erik van Velzen <erik@zenmo.com>
2023-07-04 16:00:46 +0200

    cards: less vertical padding
    The cardblock had double vertical padding leading to somewhat excessive
    whitespace on the site. Removed this.

--- frontend/components/Blocks/CardsBlock/CardBlock.tsx
+++ frontend/components/Blocks/CardsBlock/CardBlock.tsx
@@ -21,27 +21,25 @@ export default function CardBlock({
 }: Props) {
   return (
     <div className="holonContentContainer">
-      <div className="defaultBlockPadding">
-        <div
-          className={`flex flex-row justify-center flex-wrap py-12 mx-[-1rem]`}
-          data-testid="cardblock">
-          {cards.map((cardItem, index) => {
-            return (
-              <div
-                className="px-[1rem] flex-[0_0_50%] sm:flex-[0_0_33%] lg:flex-[0_0_25%] xl:flex-[0_0_20%]"
-                key={index}>
-                <Card cardItem={cardItem} cardType="cardBlockCard"></Card>
-              </div>
-            );
-          })}
-        </div>
-
-        {buttonBlock.length > 0 && (
-          <ButtonBlock
-            buttons={buttonBlock[0].value.buttons}
-            align={buttonBlock[0].value.buttonsAlign}></ButtonBlock>
-        )}
+      <div
+        className={`flex flex-row justify-center flex-wrap py-12 mx-[-1rem] defaultBlockPadding`}
+        data-testid="cardblock">
+        {cards.map((cardItem, index) => {
+          return (
+            <div
+              className="px-[1rem] flex-[0_0_50%] sm:flex-[0_0_33%] lg:flex-[0_0_25%] xl:flex-[0_0_20%]"
+              key={index}>
+              <Card cardItem={cardItem} cardType="cardBlockCard"></Card>
+            </div>
+          );
+        })}
       </div>
+
+      {buttonBlock.length > 0 && (
+        <ButtonBlock
+          buttons={buttonBlock[0].value.buttons}
+          align={buttonBlock[0].value.buttonsAlign}></ButtonBlock>
+      )}
     </div>
   );
 }
