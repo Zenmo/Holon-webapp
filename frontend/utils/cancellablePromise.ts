8c5818d93b76b621017ca8a7cf4d809769cfda4b
Erik van Velzen <erik@zenmo.com>
2023-08-24 13:42:15 +0200

    Keep KPI's in sync
    Keep KPI's in sync with inputs.
    
    Fixes #752 and improves #834.

---
+++ frontend/utils/cancellablePromise.ts
@@ -0,0 +1,23 @@
+
+export const cancellablePromise = <T>(promise: Promise<T>): [cancel: () => void, promise: Promise<T>] => {
+    let cancelled = false
+    const cancel = () => {
+        cancelled = true
+    }
+
+    return [
+        cancel,
+        new Promise((resolve, reject) => {
+            promise.then((result) => {
+                if (!cancelled) {
+                    resolve(result)
+                }
+            })
+            promise.catch((error) => {
+                if (!cancelled) {
+                    reject(error)
+                }
+            })
+        })
+    ]
+}
