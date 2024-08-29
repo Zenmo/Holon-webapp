3a7147f9217bf24fa492b6512207d2fe372f1578
Erik van Velzen <erik@zenmo.com>
2024-08-29 11:40:06 +0200

    Fix frontend test

--- src/main/tests/test_no_pending_migrations.py
+++ src/main/tests/test_no_pending_migrations.py
@@ -6,7 +6,9 @@ from django.core.management import call_command
 
 
 class NoPendingMigrationsTest(TestCase):
-    @skip("With Wagtail this is too much of a pain. Fixing this would create huge migrations which don't change anything")
+    @skip(
+        "With Wagtail this is too much of a pain. Fixing this would create huge migrations which don't change anything"
+    )
     def test_migrations(self):
         output = StringIO()
         call_command("makemigrations", interactive=False, dry_run=True, stdout=output)
