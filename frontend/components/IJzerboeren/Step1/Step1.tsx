a684684b07377d9b461ffe5be16b801a7f78963a
Erik van Velzen <erik@zenmo.com>
2025-03-06 17:35:29 +0100

    plotly sankey mock

--- frontend/components/IJzerboeren/Step1/Step1.tsx
+++ frontend/components/IJzerboeren/Step1/Step1.tsx
@@ -3,18 +3,17 @@ import { GridLoadKpi } from "@/components/KPIDashboard/KpiItems/GridLoadKpi"
 import { CostKpi } from "@/components/KPIDashboard/KpiItems/CostKpi"
 import { SustainabilityKpi } from "@/components/KPIDashboard/KpiItems/SustainabilityKpi"
 import { HeatingTypeRadios } from "@/components/IJzerboeren/Step1/HeatingTypeRadios"
-import { HeatingType, step1Data, Step1Outputs } from "@/components/IJzerboeren/Step1/step-1-data"
+import {HeatingType, SankeyLink, step1Data, Step1Outputs} from "@/components/IJzerboeren/Step1/step-1-data"
 import { useStateWithHistory } from "@/components/IJzerboeren/useStateWithHistory"
 import {TwoColumnSimulationLayout} from "@/components/Blocks/SectionBlock/TwoColumn"
 import {FunctionComponent, useLayoutEffect, useRef, useState} from "react"
-import {Sankey, Tooltip} from "recharts"
 import {uniq} from "lodash"
-import {findSingle} from "@/utils/arrayFindSingle"
+import Plotly, {SankeyData} from "plotly.js-dist-min"
 
 /**
  * Convert our format to the format expected by recharts
  */
-function convertSankeyDataToRecharts(links: any[]) {
+function convertSankeyDataToRecharts(links: SankeyLink[]) {
     const strings: string[] = links.flatMap(link => [link.source, link.target])
     const uniqueStrings = uniq(strings)
     const nodes = uniqueStrings.map(str => ({
@@ -30,6 +29,37 @@ function convertSankeyDataToRecharts(links: any[]) {
     return {
         nodes: nodes,
         links: numericLinks,
+    }
+}
+
+function convertSankeyDataToPlotly(links: SankeyLink[]): Partial<SankeyData> {
+    return {
+        type: "sankey",
+        orientation: "h",
+        node: {
+            pad: 15,
+            thickness: 30,
+            line: {
+                color: "black",
+                width: 0.5
+            },
+            label: ["A1", "A2", "B1", "B2", "C1", "C2"],
+            color: ["blue", "blue", "blue", "blue", "blue", "blue"]
+        },
+        link: {
+            source: [0,1,0,2,3,3],
+            target: [2,3,3,4,4,5],
+            value:  [8,4,2,8,4,2]
+        }
+    }
+}
+
+const plotlySankeyLayout = {
+    title: {
+        text: "Basic Sankey"
+    },
+    font: {
+        size: 10
     }
 }
 
@@ -50,14 +80,19 @@ export const Step1: FunctionComponent = () => {
     const previousKpis = previousOutputs?.kpis
 
     const rightColumnRef = useRef<HTMLDivElement | null>(null)
-
+    const sankeyDivId = "sankeyStep1" // todo make non-unique
     const [sankeyWidth, setSankeyWidth] = useState(100);
     useLayoutEffect(() => {
-        const width = rightColumnRef.current?.clientWidth;
-        if (width) {
-            setSankeyWidth(width);
+        if (!currentOutputs) {
+            return
         }
-    }, []);
+
+        Plotly.react(sankeyDivId, [convertSankeyDataToPlotly(currentOutputs.sankey)], plotlySankeyLayout)
+        // const width = rightColumnRef.current?.clientWidth;
+        // if (width) {
+        //     setSankeyWidth(width);
+        // }
+    }, [currentOutputs]);
 
     if (currentOutputs?.sankey) {
         console.log(convertSankeyDataToRecharts(currentOutputs.sankey))
@@ -75,9 +110,7 @@ export const Step1: FunctionComponent = () => {
                     justifyContent: "space-between",
                 }}>
                     {currentOutputs &&
-                        <Sankey nodePadding={50} width={sankeyWidth} height={500} data={convertSankeyDataToRecharts(currentOutputs.sankey)} link={{ stroke: '#77c878' }}>
-                            <Tooltip />
-                        </Sankey>
+                        <div id="sankeyStep1"></div>
                     }
                     <KpiRow>
                         <GridLoadKpi
