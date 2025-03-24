629872d5a452dda6ad10566a6ee201235e82a4a4
Erik van Velzen <erik@zenmo.com>
2025-03-24 15:06:39 +0100

    IJzerboeren step 3 text

--- frontend/components/IJzerboeren/Step3/Step3.tsx
+++ frontend/components/IJzerboeren/Step3/Step3.tsx
@@ -38,10 +38,30 @@ export const Step3: FunctionComponent = () => {
         <div className="holonContentContainer">
             <TwoColumnSimulationLayout style={{minHeight: "50rem"}}>
                 <div className={rawHtml.Container}>
+                    <p>Omdat een warmtenet op restwarmte hier niet mogelijk is, zou dit in potentie een geschikte casus
+                        zijn voor een warmtenet met verbranding van ijzerpoeder. Doordat de verbrander van ijzerpoeder
+                        in de buurt komt te staan zal het warmtenet minder transportverliezen hebben dan in de situatie
+                        met restwarmte. </p>
                     <HeatingTypeRadios
                         heatingType={heatingType}
-                        setHeatingType={setHeatingType} c
-                        ontentId="heatingTypeStep3" />
+                        setHeatingType={setHeatingType}
+                        contentId="heatingTypeStep3"/>
+                    <p>Selecteer je de optie ‘warmtenet met ijzerpoeder’ dan zie je zeer vergelijkbare resultaten als
+                        bij die van het warmtenet met restwarmte. Het verandert namelijk vooral de keten vooraf, niet de
+                        dynamieken in de huishoudens. Het grote verschil is dat dit warmtenet wel te realiseren is in de
+                        nieuwbouwwijk. Ten opzichte van de warmtepompen zal het de netbelasting met bijna 30% verlagen,
+                        en ten opzichte van gas ligt de CO2 uitstoot van de huizen bijna 40% lager. </p>
+                    <p>Als laatste is het kostenplaatje uiteraard belangrijk. Hoewel de LCOE voor warmtesystemen sterk
+                        situatie afhankelijk is, hebben we voor restwarmte, warmtepompen, en gas, respectievelijk, de
+                        kentallen 0.22, 0.12, en 0.15 euro/kWh gebruikt. Voor ijzerpoeder gebruiken we de
+                        ijzerpoederketen rekentool (IPA tool – link) ontwikkelt in het TSE project ‘Met Energie de Boer
+                        Op’. Daarbij komt de LCEO van verwarmen met ijzerpoeder uit op circa 0.17 eruo/kWh. Daar bovenop
+                        houden we ook nog rekening met 0.03 euro/kWh voor de lokale infrastructuur</p>
+                    <img src="/imgs/lcoe-ijzerpoeder.png" alt="LCOE opbouw van warmte opwekken met ijzerpoeder" />
+                    <p></p>
+                    <p>Voor de geintereseerde sturen raden we aan om de <a href="https://example.org">IPA tool</a> te verkennen. Daar kunnen verschillende
+                        systeem configuraties voor de productie van ijzerpoeder in detail verkend en geoptimilaseerd
+                        worden.</p>
                 </div>
                 <div style={{
                     flexDirection: "column",
