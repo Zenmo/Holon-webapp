a2113cdfa7edd9faccf712e902983475507eb282
Erik van Velzen <erik@zenmo.com>
2025-03-27 11:20:23 +0100

    Make row block responsive

--- frontend/components/Blocks/RowBlock.tsx
+++ frontend/components/Blocks/RowBlock.tsx
@@ -1,12 +1,17 @@
 import {FunctionComponent} from "react"
 import {RowBlockVariant} from "@/containers/types"
 import {ColumnBlock} from "@/components/Blocks/ColumnBlock"
+import {md} from "@/styles/breakpoints"
 
 export const RowBlock: FunctionComponent<RowBlockVariant> = (rowBlockProps) => (
-    <div style={{
+    <div css={{
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
         gap: "2.5rem",
+        [md]: {
+            flexDirection: "row",
+            justifyContent: "center",
+        },
     }} className="defaultBlockPadding holonContentContainer" >
         {rowBlockProps.value.columns.map(
             columnProps => <ColumnBlock key={columnProps.id} {...columnProps} />
